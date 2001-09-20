22b1d7bcef9e2192ea79724ad27c90138cd090da
Matt Rippa <m.rippa@jach.hawaii.edu>
2001-09-20 01:36:31 +0000

    First Addition of OMP-OM.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1043 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/OmpOM.java
+++ src/main/edu/jach/qt/gui/OmpOM.java
@@ -1,41 +1,45 @@
 package edu.jach.qt.gui;
 
-/**
- * OmpOM.java
- *
- *
- * Created: Wed Sep  5 19:12:04 2001
- *
- * @author <a href="mailto: mrippa@jach.hawaii.edu"Mathew Rippa</a>
- */
-
-import javax.swing.*;
-import javax.swing.event.*;
-import java.io.FileInputStream;
-import java.io.DataInputStream;
-import java.util.Properties;
-import jsky.app.ot.OtCfg;
-import jsky.app.ot.OtFileIO;
-import orac.util.*;
-import orac.ukirt.inst.*;
-import orac.ukirt.iter.*;
-import gemini.sp.iter.*;
-import gemini.sp.obsComp.*;
-import gemini.sp.SpRootItem;
 import java.awt.Dimension;
 import java.io.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import edu.jach.qt.utils.*;
+
 import gemini.sp.SpItem;
+import gemini.sp.SpRootItem;
+import gemini.sp.iter.*;
+import gemini.sp.obsComp.*;
+import jsky.app.ot.OtCfg;
+import jsky.app.ot.OtFileIO;
+import orac.ukirt.inst.*;
+import orac.ukirt.iter.*;
+import orac.util.*;
 
-
+/**
+ * This is the top most class in the <code>OmpOM</code>.  This 
+ * starts off all subsequent OMP-OM specific classes.
+ *
+ * @author <a href="mailto:mrippa@jach.hawaii.edu">Mathew Rippa</a>
+ * $Id$
+ */
 public class OmpOM extends JFrame{
+   /**
+    * Creates a new <code>OmpOM</code> instance.
+    *
+    */
    public OmpOM (){
       setSize(new Dimension(300, 400));
       setTitle("OMP --OBSERVATION MANAGER--");
-      OtCfg.init();
+      //OtCfg.init();
       OtFileIO.setXML(System.getProperty("OMP") != null);
 
       //Load OM specific configs
-      loadConfig(new String("/home/mrippa/root/install/omp/QT/config/om.cfg"));
+      QtTools.loadConfig(new String("/home/mrippa/netroot/install/omp/QT/config/om.cfg"));
 
       // Need to construct UKIRT-specific items so that their SpTypes are
       // statically initialised.  Otherwise the sp classes won't know about 
@@ -65,7 +69,7 @@ public class OmpOM extends JFrame{
    }
 
    private void startOM() {
-      File file = new File("/home/mrippa/root/src/omp/omp/QT/config/msb.xml");
+      File file = new File("/home/mrippa/netroot/install/omp/QT/config/msb.xml");
       String dir = file.getParent();
       String name = file.getName();
       SpRootItem spItem = OtFileIO.fetchSp(dir, name);
@@ -76,50 +80,5 @@ public class OmpOM extends JFrame{
 
       ptree.addTree(spItem);
    }
-
-   /** private static void loadConfig(String filename) is a private method
-    reads a configuration file and set up configuration. It is done by
-    putting things into java system properties which is program-system-wide visible.
-
-    @param String args
-    @return  none
-    @throws none
-   */
-   private static void loadConfig(String filename) {
-      try {
-	 String line_str;
-	 int line_number;
-	 FileInputStream is = new FileInputStream(filename);
-	 DataInputStream ds = new DataInputStream(is);
-
-	 Properties temp=System.getProperties();
-	 int lineno = 0;
-
-	 while ((line_str = ds.readLine()) != null) {
-	    lineno++;
-	    if(line_str.length()>0) {
-	       if(line_str.charAt(0)=='#') continue;
-
-	       try {
-		  int colonpos = line_str.indexOf(":");
-		  temp.put(line_str.substring(0,colonpos).trim(),
-			   line_str.substring(colonpos+1).trim());
-
-	       }catch (IndexOutOfBoundsException e) {
-		  System.out.println ("Problem reading line "+lineno+": "+line_str);
-		  ds.close();
-		  is.close();
-		  System.exit(1);
-	       }
-	    }
-	 }
-	 ds.close();
-	 is.close();
-
-      } catch (IOException e) {
-	 System.out.println("File error: " + e);
-      }
-   }
-
 
 }// OmpOM
