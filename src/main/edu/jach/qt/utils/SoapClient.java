6c7443abbf11a08466a52b2a9238f76dabfa6976
Matt Rippa <m.rippa@jach.hawaii.edu>
2001-11-10 01:26:27 +0000

    Now the doCall() method returns back an Object, not a String.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1296 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/utils/SoapClient.java
+++ src/main/edu/jach/qt/utils/SoapClient.java
@@ -6,6 +6,9 @@ import java.net.URL;
 import java.util.Vector;
 import org.apache.soap.*;
 import org.apache.soap.rpc.*;
+import org.apache.soap.encoding.SOAPMappingRegistry;
+import org.apache.soap.encoding.literalxml.XMLParameterSerializer;
+import org.apache.soap.util.xml.QName;
 
 
 /**
@@ -19,47 +22,66 @@ import org.apache.soap.rpc.*;
  */
 public class SoapClient {
 
-   private static Header header = null;
-   private static Vector params = new Vector();
+    private static Header header = null;
+    private static Vector params = new Vector();
+   
+    protected static void addParameter(String name, Class type, Object val) {
+	params.add(new Parameter(name, type, val, null));
+    }
+    
+    protected static Object doCall(URL url, String soapAction, String methodName)  {
+       //String result = "";
+       Object obj = new Object();
 
-   protected static void addParameter(String name, Class type, Object val) {
-      params.add(new Parameter(name, type, val, null));
-   }
+       //Next 3 statements for getting a struct?
+       //SOAPMappingRegistry smr = new SOAPMappingRegistry();
+       //XMLParameterSerializer xmlSer = new XMLParameterSerializer();
+       //smr.mapTypes(Constants.NS_URI_SOAP_ENC,new
+       //	  QName("http://www.w3.org/1999/XMLSchema", "Struct"),
+       //	    org.w3c.dom.Element.class, xmlSer, xmlSer);
+       
+	try {
+	    Call call = new Call();
 
-   protected static String doCall(URL url, String soapAction, String methodName)  {
-      String result = "";
-      try {
-	 Call call = new Call();
-	 call.setTargetObjectURI(soapAction);
-	 call.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
-	 call.setMethodName(methodName);
-	 call.setParams(params);
-	 if (header != null)
-	    call.setHeader(header);
-	 
-	 soapAction +="#" + methodName;
-	 Response resp = call.invoke(url, soapAction);
-      
-	 // check response 
-	 if (!resp.generatedFault()) {
-	    Parameter ret = resp.getReturnValue();
-	    result = (String) ret.getValue();
-	 }
-	 else {
-	    Fault fault = resp.getFault ();
-	    System.err.println (methodName + " generated fault: ");
-	    System.out.println ("  Fault Code   = " + fault.getFaultCode());  
-	    System.out.println ("  Fault String = " + fault.getFaultString());
-	 }
-      
-      } catch (Exception e) {
-	 e.printStackTrace();
-      }
+	    //The next line with the above code to get a struct
+	    //call.setSOAPMappingRegistry(smr);
 
-      //Reset the params vector.
-      params.clear();
+	    call.setTargetObjectURI(soapAction);
+	    call.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
+	    call.setMethodName(methodName);
+	    call.setParams(params);
+	    if (header != null)
+		call.setHeader(header);
+	    soapAction +="#" + methodName;
 
-      return result;
-   }
+	    //Let's dump the envelope
+	    //Envelope env = call.buildEnvelope();
+	    //System.out.println(env.toString());
+	    
+	    Response resp = call.invoke(url, soapAction);
+
+	    // check response 
+	    if (!resp.generatedFault()) {
+		Parameter ret = resp.getReturnValue();
+		//result = (String) ret.getValue();
+		obj = ret.getValue();
+	    }
+	    else {
+		Fault fault = resp.getFault ();
+		System.err.println (methodName + " generated fault: ");
+		System.out.println ("  Fault Code   = " + fault.getFaultCode());  
+		System.out.println ("  Fault String = " + fault.getFaultString());
+	    }
+	    
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	
+	//Reset the params vector.
+	params.clear();
+	
+	//return result;
+	return obj;
+    }
 } // SoapClient
 
