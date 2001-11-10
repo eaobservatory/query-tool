67d7975465298b59ad1d05f12697102815418f86
Matt Rippa <m.rippa@jach.hawaii.edu>
2001-11-10 01:01:50 +0000

    Now adds a method to add a drag-n-drop tree to the right pane.  Alternatively you can add just CalibrationArea object.  Also, new is the test method that just runs the OmpOM up by itself.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1289 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/OmpOM.java
+++ src/main/edu/jach/qt/gui/OmpOM.java
@@ -1,24 +1,31 @@
 package edu.jach.qt.gui;
 
-import java.awt.Dimension;
-import java.io.*;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.util.Properties;
-import javax.swing.*;
-import javax.swing.event.*;
 
 import edu.jach.qt.utils.*;
-
 import gemini.sp.SpItem;
 import gemini.sp.SpRootItem;
 import gemini.sp.iter.*;
 import gemini.sp.obsComp.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.Properties;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import jsky.app.ot.FileInfo;
 import jsky.app.ot.OtCfg;
 import jsky.app.ot.OtFileIO;
+import jsky.app.ot.OtTreeWidget;
+import jsky.app.ot.OtWindow;
 import orac.ukirt.inst.*;
 import orac.ukirt.iter.*;
 import orac.util.*;
+import javax.swing.JSplitPane;
+import edu.jach.qt.gui.DragDropObject;
+import javax.swing.border.*;
 
 /**
  * This is the top most class of the OMP-OM.  This 
@@ -27,15 +34,19 @@ import orac.util.*;
  * @author <a href="mailto:mrippa@jach.hawaii.edu">Mathew Rippa</a>
  * $Id$
  */
-public class OmpOM extends JFrame{
+public class OmpOM extends JPanel{
+
+
+   private ProgramTree	      ptree;
+   private File		      file;
+   private SpRootItem	      spRootItem;
+
    /**
     * Creates a new <code>OmpOM</code> instance.
     *
     */
    public OmpOM (){
-      setSize(new Dimension(300, 400));
-      setTitle("OMP --OBSERVATION MANAGER--");
-      //OtCfg.init();
+
       OtFileIO.setXML(System.getProperty("OMP") != null);
 
       //Load OM specific configs
@@ -69,16 +80,83 @@ public class OmpOM extends JFrame{
    }
 
    private void startOM() {
-      File file = new File(System.getProperty("msbFile"));
-      String dir = file.getParent();
-      String name = file.getName();
-      SpRootItem spItem = OtFileIO.fetchSp(dir, name);
 
-      ProgramTree ptree = new ProgramTree();
-      getContentPane().add(ptree);
-      show();
+      file = new File(System.getProperty("msbFile"));
+      spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
 
-      ptree.addTree(spItem);
+      ptree = new ProgramTree();
+      ptree.addTree(spRootItem);
+      ptree.setMinimumSize(new Dimension(400,550) );
+
    }
 
+   public void resetTree() {
+      spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
+      ptree.addTree(spRootItem);
+   }
+
+   public JSplitPane getTreePanel() {
+      
+      CalibrationArea ca = new CalibrationArea();
+      JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
+					    ptree, ca);
+      return splitPane;
+   }
+
+   public JSplitPane getDragTreePanel() {
+      
+      DragDropObject ddo = new DragDropObject(spRootItem);
+      MsbNode root = new MsbNode(ddo);
+      getItems(spRootItem, root);
+
+      DnDJTree ddt = new DnDJTree(root);
+      ddt.setCellRenderer(new DragTreeCellRenderer());
+      //JPanel p = new JPanel();
+      JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
+					    ptree, ddt);
+
+      return splitPane;
+   }
+
+   public void test() {
+      
+      JFrame f = new JFrame();
+      f.setSize(400,300);
+      DragDropObject ddo = new DragDropObject(spRootItem);
+      MsbNode root = new MsbNode(ddo);
+      getItems(spRootItem, root);
+      DnDJTree ddt = new DnDJTree(root);
+      ddt.setCellRenderer(new MyTreeCellRenderer());
+      f.getContentPane().add(ddt);
+      //f.pack();
+      f.show();
+   }
+
+
+   /** public void getItems (SpItem spItem,DefaultMutableTreeNode node)
+       is a public method to add ALL the items of a sp object into the
+       JTree *recursively*.
+      
+       @param SpItem spItem,DefaultMutableTreeNode node
+       @return  none
+       @throws none
+      
+   */
+   private void getItems (SpItem spItem, MsbNode node) {
+      Enumeration children = spItem.children();
+      while (children.hasMoreElements()) {
+	 
+	 SpItem  childNode = (SpItem) children.nextElement();
+	 DragDropObject ddo = new DragDropObject(childNode);
+	 MsbNode temp = new MsbNode(ddo);
+	 node.add(temp);
+	 getItems(childNode,temp);
+      }
+   }
+
+
+   public static void main(String[] args) {
+      QtTools.loadConfig("/home/mrippa/netroot/install/omp/QT/config/qt.conf");
+      OmpOM om = new OmpOM();
+   }
 }// OmpOM
