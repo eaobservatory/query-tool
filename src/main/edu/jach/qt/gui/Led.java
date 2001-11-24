fd731c598ff3e2084f427e4ee3ebedd549c3f6be
Matt Rippa <m.rippa@jach.hawaii.edu>
2001-11-24 03:59:15 +0000

    This is a monitor for the SwingWorker thread that goes off to process SOAP requests.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1346 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/gui/Led.java
@@ -0,0 +1,88 @@
+package edu.jach.qt.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.net.*;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.Timer;
+import javax.swing.border.*;
+import jsky.util.gui.BasicWindowMonitor;
+
+/**
+ * Led.java
+ *
+ *
+ * Created: Wed Nov 21 13:28:52 2001
+ *
+ * @author <a href="mailto: mrippa@jach.hawaii.edu"Mathew Rippa</a>
+ */
+
+public class Led extends JPanel {
+
+  /** Handle for the "LED" button */
+  protected JButton led;
+
+  /** Action linked to the stop button and led */
+  protected AbstractAction stopAction = new AbstractAction("Stop") {
+      public void actionPerformed(ActionEvent evt) {
+	this.setEnabled(false);
+      }
+    };
+  
+  /** Timer used for blinking the led */
+  protected Timer timer;
+
+  /** The current state of the led (for blinking) */
+  protected boolean ledState;
+
+  public Led () {
+    setBackground(Color.black);
+    add(createLed());
+    led.setEnabled(false);
+  }
+
+
+  /**
+     * Create the red LED button
+     */
+  protected JButton createLed() {
+    ClassLoader cl = ClassLoader.getSystemClassLoader();
+    URL url = cl.getResource("green_led.gif");
+    led = new JButton(new ImageIcon(url));
+    led.setBackground(Color.black);
+    led.setFocusPainted(false);
+    led.setBorderPainted(false);
+
+    url = cl.getResource("green_led_disabled.gif");
+    led.setDisabledIcon(new ImageIcon(url));
+    led.addActionListener(stopAction);
+
+    return led;
+  }
+
+  protected void blinkLed(boolean enabled) {
+    led.setEnabled(ledState = enabled);
+    if (enabled) {
+      if (timer == null) {
+	timer = new Timer(200, new ActionListener() {
+	    public void actionPerformed(ActionEvent ev) {
+	      ledState = !ledState;
+	      led.setEnabled(ledState);
+	    }
+	  });
+	timer.start();
+      }
+      else {
+	timer.restart();
+      }
+    } 
+    else {
+      if (timer != null) {
+	timer.stop();
+      }
+    }
+  }
+  
+}// Led
