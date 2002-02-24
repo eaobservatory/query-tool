7ae07c71afbe29bcd0f00df3623fc9bd3a739787
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-02-24 06:53:39 +0000

    Cleaned up imports.  Added support for all widgets seen in the InputPanel.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1837 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/app/Querytool.java
+++ src/main/edu/jach/qt/app/Querytool.java
@@ -1,19 +1,13 @@
 package edu.jach.qt.app;
 
+import edu.jach.qt.gui.*;
+import edu.jach.qt.utils.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
-
-import edu.jach.qt.gui.WidgetDataBag;
-import edu.jach.qt.utils.*;
-import org.apache.xerces.dom.DOMImplementationImpl;
-import org.apache.xerces.dom.DocumentImpl;
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.Serializer;
-import org.apache.xml.serialize.SerializerFactory;
-import org.apache.xml.serialize.XMLSerializer;
+import org.apache.xerces.dom.*;
+import org.apache.xml.serialize.*;
 import org.w3c.dom.*;
-import org.w3c.dom.Document;
 
 /**
  * The <code>Querytool</code> is main driver for the application side
@@ -34,165 +28,249 @@ import org.w3c.dom.Document;
  */
 public class Querytool implements Runnable, Observer {
 
-   private String _xmlString;
-   private WidgetDataBag bag;
+  private String _xmlString;
+  private WidgetDataBag bag;
 
-   /**
-    * Creates a new <code>Querytool</code> instance.
-    *
-    * @param bag a <code>WidgetDataBag</code> value
-    */
-   public Querytool (WidgetDataBag bag) {
-      this.bag = bag;
-      bag.addObserver(this);
-   }
+  /**
+   * Creates a new <code>Querytool</code> instance.
+   *
+   * @param bag a <code>WidgetDataBag</code> value
+   */
+  public Querytool (WidgetDataBag bag) {
+    this.bag = bag;
+    bag.addObserver(this);
+  }
 
-   /**
-    * The <code>update</code> method is used to trigger
-    * an action if a change is "Observed" in the "Subject".
-    * This is the only method mandated by the Observer 
-    * interface.
-    *
-    * @param o a <code>Subject</code> value
-    */
-   public void update(Subject o) {
-      if (o == bag) {
-	 buildXML(bag.getHash());
-      }
-   }
+  /**
+   * The <code>update</code> method is used to trigger
+   * an action if a change is "Observed" in the "Subject".
+   * This is the only method mandated by the Observer 
+   * interface.
+   *
+   * @param o a <code>Subject</code> value
+   */
+  public void update(Subject o) {
+    if (o == bag) {
+      buildXML(bag.getHash());
+    }
+  }
 
-   /**
-    * The <code>buildXML</code> method is triggerd by any 
-    * Subject update.  If the gui state changes, this method
-    * rebuilds the _xmlString.
-    *
-    * @param ht a <code>Hashtable</code> value
-    */
-   public void buildXML(Hashtable ht) {
-      try {
-	 String next = "";
-	 Document doc = new DocumentImpl();
-	 Element root = doc.createElement("query");
-	 Element item, sub;
-	 JToggleButton abstractButton;
-	 Object obj;
+  /**
+   * The <code>buildXML</code> method is triggerd by any 
+   * Subject update.  If the gui state changes, this method
+   * rebuilds the _xmlString.
+   *
+   * @param ht a <code>Hashtable</code> value
+   */
+  public void buildXML(Hashtable ht) throws NullPointerException {
+    try {
+      String next = "";
+      Document doc = new DocumentImpl();
+      Element root = doc.createElement("MSBQuery");
+      Element item, sub;
+      JToggleButton abstractButton;
+      Object obj;
+
+      item = doc.createElement("telescope");
+      item.appendChild( doc.createTextNode(System.getProperty("telescope")) );
+      root.appendChild(item);
 	 
-	 for(Enumeration e = ht.keys(); e.hasMoreElements() ; ) {
-	    next = (String) e.nextElement();
-	    item = doc.createElement(next);
-	    if (next.equalsIgnoreCase("Instruments")) {
-	       for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
-		    iter.hasNext(); 
-		    iter.nextIndex()) {
-		  abstractButton = (JCheckBox) (iter.next());
-		  if (abstractButton.isSelected()) {
-		     sub = doc.createElement("instrument");
-		     sub.appendChild( doc.createTextNode( abstractButton.getLabel() ));
-		     item.appendChild(sub);
-		  }
-	       }
-	    }
-	    else if(next.equalsIgnoreCase("Moon")) {
-	       for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
-		    iter.hasNext(); 
-		    iter.nextIndex()) {
-		     abstractButton = (JRadioButton) (iter.next());
-		     if (abstractButton.isSelected()) {
-			item.appendChild( doc.createTextNode(abstractButton.getLabel() ));
-		     }
-	       }
-	    }
-	    else if (ht.get(next) instanceof LinkedList ) {
-	       for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
-		    iter.hasNext(); 
-		    iter.nextIndex()) {
+      for(Enumeration e = ht.keys(); e.hasMoreElements() ; ) {
+	next = ((String)e.nextElement());
 
-		  obj = iter.next();
-		  if (obj instanceof JComboBox) {
-		     String textField = (String) (iter.next());
-		     obj = (JComboBox)obj;
-		     if(!textField.equals("")) {
-		     sub = doc.createElement((String) (((JComboBox)obj).getSelectedItem())) ;
-		     sub.appendChild( doc.createTextNode(textField));
-		     item.appendChild(sub);
-		     }
-		  }
-	       }
+	if (next.equalsIgnoreCase("instruments")) {
+	  for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
+	       iter.hasNext(); 
+	       iter.nextIndex()) {
+	    abstractButton = (JCheckBox) (iter.next());
+	    if (abstractButton.isSelected()) {
+	      item = doc.createElement("instrument");
+	      item.appendChild( doc.createTextNode( abstractButton.getText() ));
+	      root.appendChild(item);
 	    }
-	    else {
-	       //
-	       item.appendChild( doc.createTextNode((String)ht.get(next)) );
-	       //
-	    }
+	  }
+	}
+	else if(next.equalsIgnoreCase("Moon")) {
 
+	  item = doc.createElement(next);
+	  for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
+	       iter.hasNext(); 
+	       iter.nextIndex()) {
+	    abstractButton = (JRadioButton) (iter.next());
+	    if (abstractButton.isSelected()) {
+	      String tmpMoon = abstractButton.getText();
+	      String moon = "";
+	      if ( tmpMoon.equals("Dark")) {
+		moon = "0";
+	      }
+	      else if (tmpMoon.equals("Bright")) {
+		moon = "1";
+	      }
+	      else {
+		moon = "2";
+	      } // end of else
+		  
+	      item.appendChild(doc.createTextNode(moon));
+	    }
+	  }
+	}
+	else if(next.equalsIgnoreCase("Atmospheric Conditions")) {
+	  item = doc.createElement(next);
+	  for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
+	       iter.hasNext(); 
+	       iter.nextIndex()) {
+
+	    //abstractButton = (JRadioButton) (iter.next());
+	    System.out.println("ATMOS: "+(String)iter.next());
+		
+	    //  		if (abstractButton.isSelected()) {
+	    //  		  String seeing = abstractButton.getText();
+	    //  		  item.appendChild(doc.createTextNode(moon));
+	  }
+	}
+	else if (ht.get(next) instanceof LinkedList ) {
+	  item = doc.createElement(next);
+	  for (ListIterator iter = ((LinkedList)(ht.get(next))).listIterator(0); 
+	       iter.hasNext(); 
+	       iter.nextIndex()) {
+		
+	    obj = iter.next();
+	    if (obj instanceof JComboBox) {
+	      String textField = (String) (iter.next());
+	      obj = (JComboBox)obj;
+	      if(!textField.equals("")) {
+		sub = doc.createElement((String) (((JComboBox)obj).getSelectedItem())) ;
+		sub.appendChild( doc.createTextNode(textField));
+		item.appendChild(sub);
+	      }
+	    }
+	  }
+	}
+	else if (ht.get(next) instanceof LabeledTextField ) {
+	  LabeledTextField ltf = (LabeledTextField) (ht.get(next));
+	  Enumeration n = ltf.getList().elements();
+	  String tmpStr;
+	  item = doc.createElement(next);
+
+	  while (n.hasMoreElements()) {
+	    if ( !next.equalsIgnoreCase("PICI"))
+	      item = doc.createElement(next);
+	    else
+	      item = doc.createElement("name");
+
+	    tmpStr = (String)n.nextElement();
+	    item.appendChild(doc.createTextNode(tmpStr.trim()));
 	    root.appendChild( item );
-	 }
-	 doc.appendChild(root);
+	  } // end of while ()
+	}
 
-	 OutputFormat    format  = new OutputFormat( doc,"UTF-8",true );   //Serialize DOM
-	 StringWriter  stringOut = new StringWriter();        //Writer will be a String
-	 XMLSerializer    serial = new XMLSerializer( stringOut, format );
-	 serial.asDOMSerializer();                            // As a DOM Serializer
-	 serial.serialize( doc.getDocumentElement() );
-	 
-	 _xmlString = stringOut.toString();
-      } catch ( Exception ex ) {
-	 ex.printStackTrace();
+	else if (ht.get(next) instanceof LabeledRangeTextField ) {
+	  
+	  LabeledRangeTextField lrtf = (LabeledRangeTextField) (ht.get(next));
+	  String tmpStr;
+
+	  if ( next.equalsIgnoreCase("DUR")) {
+	    item = doc.createElement("timeest");
+	    item.setAttribute("units","minutes");
+	  } else {
+	    item = doc.createElement(next);
+	  }
+	  
+	  tmpStr = lrtf.getLowerText();
+	  sub = doc.createElement("min") ;
+	  sub.appendChild( doc.createTextNode(tmpStr.trim().toLowerCase()));
+	  item.appendChild(sub);
+
+	
+	  tmpStr = lrtf.getUpperText();
+	  sub = doc.createElement("max");
+	  sub.appendChild(doc.createTextNode(tmpStr.trim().toLowerCase()));
+	  item.appendChild(sub);
+
+	  root.appendChild( item );
+	}
+	
+	else if (next.equalsIgnoreCase("pc" )) {
+	  item = doc.createElement("photometric");
+	  item.appendChild( doc.createTextNode((String)ht.get(next)));
+	}
+	
+	
+	else {
+	  item = null;
+	  throw (new NullPointerException("A widget in the InputPanel has data, but has not been set!"));
+	} // end of else
+	
+
+	root.appendChild( item );
       }
-   }
 
-   /**
-    * The <code>getXML</code> method returns the _xmlString.
-    *
-    * @return a <code>String</code> value
-    */
-   public String getXML() {
-      return _xmlString;
-   }
+      doc.appendChild(root);
 
-   /**
-    * The <code>printXML</code> method is a utility to the current
-    * _xmlString.
-    */
-   public void printXML() {
-      System.out.println( _xmlString ); //Spit out DOM as a String
-   }
+      OutputFormat    format  = new OutputFormat( doc,"UTF-8",true );   //Serialize DOM
+      StringWriter  stringOut = new StringWriter();        //Writer will be a String
+      XMLSerializer    serial = new XMLSerializer( stringOut, format );
+      serial.asDOMSerializer();                            // As a DOM Serializer
+      serial.serialize( doc.getDocumentElement() );
+	 
+      _xmlString = stringOut.toString();
+    } catch ( Exception ex ) {
+      ex.printStackTrace();
+    }
+  }
 
-   /**
-    * The <code>queryMSB</code> method starts the SOAP client.
-    * A successful query will write all MSB Summaries to file.
-    */
-   public void run() {
-      //QuerytoolClient qtc = new QuerytoolClient();
-      MsbClient.queryMSB(_xmlString);
-   }
+  /**
+   * The <code>getXML</code> method returns the _xmlString.
+   *
+   * @return a <code>String</code> value
+   */
+  public String getXML() {
+    return _xmlString;
+  }
 
-   /**
-    * The <code>queryMSB</code> method starts the SOAP client.
-    * A successful query will write all MSB Summaries to file and 
-    * return true.
-    */
-   public boolean queryMSB() {
-     return MsbClient.queryMSB(_xmlString);
-   }
+  /**
+   * The <code>printXML</code> method is a utility to the current
+   * _xmlString.
+   */
+  public void printXML() {
+    System.out.println( _xmlString ); //Spit out DOM as a String
+  }
 
-   /**
-    * The <code>fetchMSB</code> method starts the SOAP client.
-    * A successful fetch will start the lower level OMP-OM sequence.
-    *
-    * @param i an <code>Integer</code> value
-    */
-   public void fetchMSB(Integer i) {
-      MsbClient.fetchMSB(i);
-   }
+  /**
+   * The <code>queryMSB</code> method starts the SOAP client.
+   * A successful query will write all MSB Summaries to file.
+   */
+  public void run() {
+    //QuerytoolClient qtc = new QuerytoolClient();
+    MsbClient.queryMSB(_xmlString);
+  }
 
-   /**
-    * <code>XMLisNull</code> is a test for a null _xmlString.
-    *
-    * @return a <code>boolean</code> value
-    */
-   public boolean XMLisNull() {
-      return (_xmlString == null);
-   }
+  /**
+   * The <code>queryMSB</code> method starts the SOAP client.
+   * A successful query will write all MSB Summaries to file and 
+   * return true.
+   */
+  public boolean queryMSB() {
+    return MsbClient.queryMSB(_xmlString);
+  }
+
+  /**
+   * The <code>fetchMSB</code> method starts the SOAP client.
+   * A successful fetch will start the lower level OMP-OM sequence.
+   *
+   * @param i an <code>Integer</code> value
+   */
+  public void fetchMSB(Integer i) {
+    MsbClient.fetchMSB(i);
+  }
+
+  /**
+   * <code>XMLisNull</code> is a test for a null _xmlString.
+   *
+   * @return a <code>boolean</code> value
+   */
+  public boolean XMLisNull() {
+    return (_xmlString == null);
+  }
 
 }// Querytool
