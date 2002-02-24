7d6fe0f2ab0812abccfa9a7ecf883d0b4f1cbd96
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-02-24 07:20:20 +0000

    Add the tau monitor.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1849 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/TelescopeDataPanel.java
+++ src/main/edu/jach/qt/gui/TelescopeDataPanel.java
@@ -2,9 +2,11 @@ package edu.jach.qt.gui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DecimalFormat;
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.event.*;
+import ocs.utils.DcHub;
 
 
 /**
@@ -18,83 +20,96 @@ import javax.swing.event.*;
 
 public class TelescopeDataPanel extends JPanel {
    
-   private JLabel csoTau;
-   private JLabel csoTauValue;
-   private JLabel seeing;
-   private JLabel seeingValue;
-   private JLabel airmass;
-   private JLabel airmassValue;
-   private JButton updateButton;
+  private JLabel csoTau;
+  private static JLabel csoTauValue;
+  private JLabel seeing;
+  private JLabel seeingValue;
+  private JLabel airmass;
+  private JLabel airmassValue;
+  private JButton updateButton;
 
-   public TelescopeDataPanel() {
-      csoTau = new JLabel("CSO Tau: ", JLabel.LEADING);
-      seeing = new JLabel("Seeing: ", JLabel.LEADING);
-      airmass = new JLabel("Airmass: ", JLabel.LEADING);
-      csoTauValue = new JLabel("0.08", JLabel.LEADING);
-      seeingValue = new JLabel("0.25\"", JLabel.LEADING);
-      airmassValue = new JLabel("1.2", JLabel.LEADING);
+  public static String tauString = ""+0.00;
 
-      updateButton = new JButton("Update");
+  public TelescopeDataPanel() {
+    DcHub hub = new DcHub();
+    hub.register("CSOMON");
+
+    csoTau = new JLabel("CSO Tau: ", JLabel.LEADING);
+    seeing = new JLabel("Seeing: ", JLabel.LEADING);
+    airmass = new JLabel("Airmass: ", JLabel.LEADING);
+    csoTauValue = new JLabel(tauString, JLabel.LEADING);
+    seeingValue = new JLabel("0.25\"", JLabel.LEADING);
+    airmassValue = new JLabel("1.2", JLabel.LEADING);
+
+    updateButton = new JButton("Set Current");
+    repaint();
+  }
+  
+  public static void setTau(double val) {
+    
+    DecimalFormat myFormatter = new DecimalFormat("0.000");
+    String output = myFormatter.format(val);
+
+    TelescopeDataPanel.csoTauValue.setText(""+output);
+  }
+
+  public void config() {
       
-   }
+    setBackground(Color.black);
+    setBorder(BorderFactory.createTitledBorder
+	      (BorderFactory.createEtchedBorder(Color.green, Color.black), "Current Info",
+	       TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION));
+    setLayout(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
 
-   public void config() {
-      
-      setBackground(Color.black);
-      setBorder(BorderFactory.createTitledBorder
-		(BorderFactory.createEtchedBorder(Color.green, Color.black), "Current Info",
-		 TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION));
-      setLayout(new GridBagLayout());
-      GridBagConstraints gbc = new GridBagConstraints();
+    csoTau.setForeground(java.awt.Color.white);
+    seeing.setForeground(java.awt.Color.white);
+    airmass.setForeground(java.awt.Color.white);
+    csoTauValue.setForeground(java.awt.Color.green);
+    seeingValue.setForeground(java.awt.Color.green);
+    airmassValue.setForeground(java.awt.Color.green);
 
-      csoTau.setForeground(java.awt.Color.white);
-      seeing.setForeground(java.awt.Color.white);
-      airmass.setForeground(java.awt.Color.white);
-      csoTauValue.setForeground(java.awt.Color.green);
-      seeingValue.setForeground(java.awt.Color.green);
-      airmassValue.setForeground(java.awt.Color.green);
-
-      updateButton.setBackground(java.awt.Color.gray);
+    updateButton.setBackground(java.awt.Color.gray);
 
 
-      gbc.fill = GridBagConstraints.NONE;
-      //gbc.anchor = GridBagConstraints.CENTER;
-      gbc.insets.bottom = 5;
-      gbc.insets.left = 5;
-      gbc.insets.right = 5;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
+    gbc.fill = GridBagConstraints.NONE;
+    //gbc.anchor = GridBagConstraints.CENTER;
+    gbc.insets.bottom = 5;
+    gbc.insets.left = 5;
+    gbc.insets.right = 5;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
 
-      add(csoTau, gbc, 0, 0, 1, 1);
-      add(csoTauValue, gbc, 1, 0, 1, 1);
+    add(csoTau, gbc, 0, 0, 1, 1);
+    add(csoTauValue, gbc, 1, 0, 1, 1);
 
-      add(seeing, gbc, 0, 1, 1, 1);
-      add(seeingValue, gbc, 1, 1, 1, 1);
+    add(seeing, gbc, 0, 1, 1, 1);
+    add(seeingValue, gbc, 1, 1, 1, 1);
 
-      add(airmass, gbc, 0, 2, 1, 1);
-      add(airmassValue, gbc, 1, 2, 1, 1);
+    add(airmass, gbc, 0, 2, 1, 1);
+    add(airmassValue, gbc, 1, 2, 1, 1);
 
-      add(updateButton, gbc, 0, 3, 2, 1);
+    add(updateButton, gbc, 0, 3, 2, 1);
             
-   }
+  }
    
-   /**
-    * Describe <code>add</code> method here.
-    *
-    * @param c a <code>Component</code> value
-    * @param gbc a <code>GridBagConstraints</code> value
-    * @param x an <code>int</code> value
-    * @param y an <code>int</code> value
-    * @param w an <code>int</code> value
-    * @param h an <code>int</code> value
-    */
-   public void add(Component c, GridBagConstraints gbc, 
-		   int x, int y, int w, int h) {
-      gbc.gridx = x;
-      gbc.gridy = y;
-      gbc.gridwidth = w;
-      gbc.gridheight = h;
-      add(c, gbc);      
-   }
+  /**
+   * Describe <code>add</code> method here.
+   *
+   * @param c a <code>Component</code> value
+   * @param gbc a <code>GridBagConstraints</code> value
+   * @param x an <code>int</code> value
+   * @param y an <code>int</code> value
+   * @param w an <code>int</code> value
+   * @param h an <code>int</code> value
+   */
+  public void add(Component c, GridBagConstraints gbc, 
+		  int x, int y, int w, int h) {
+    gbc.gridx = x;
+    gbc.gridy = y;
+    gbc.gridwidth = w;
+    gbc.gridheight = h;
+    add(c, gbc);      
+  }
    
 }
