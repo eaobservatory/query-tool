7d29b349ad4c85023e2b969a64a899c0898b78de
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-02-24 07:02:26 +0000

    Cleaned up imports.  Changed the "update" button to read "Set Current".
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1841 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/InfoPanel.java
+++ src/main/edu/jach/qt/gui/InfoPanel.java
@@ -1,19 +1,18 @@
 package edu.jach.qt.gui;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.border.*;
-import javax.swing.ImageIcon;
-import java.net.URL;
-import java.awt.Toolkit;
 import edu.jach.qt.app.Querytool;
 import edu.jach.qt.utils.*;
-
-import java.io.FileNotFoundException;
+import java.awt.*;
+import java.awt.Toolkit;
+import java.awt.event.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Vector;
+import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.border.*;
+import javax.swing.event.*;
 /**
  * InfoPanel.java
  *
@@ -25,236 +24,222 @@ import java.util.Vector;
  */
 
 public class InfoPanel extends JPanel
-   implements ActionListener {
+  implements ActionListener {
 
-   //reconsider icons dir 'An absolute path!'
-   private static final String LOGO_IMAGE = System.getProperty("qtLogo");
+  //reconsider icons dir 'An absolute path!'
+  private static final String LOGO_IMAGE = System.getProperty("qtLogo");
 
-   private MSBQueryTableModel msb_qtm;
-   private TimePanel timePanel ;
-   private Querytool localQuerytool;
+  private MSBQueryTableModel msb_qtm;
+  private TimePanel timePanel ;
+  private Querytool localQuerytool;
 
-   JLabel hstLabel = new JLabel("HST");
-   JButton searchButton = new JButton();
-   JButton xmlPrintButton = new JButton();
-   JButton exitButton = new JButton();
-   JButton fetchMSB   = new JButton();
-   QtFrame qtf;
-   JLabel logoImage;
+  JLabel hstLabel = new JLabel("HST");
+  JButton searchButton = new JButton();
+  JButton xmlPrintButton = new JButton();
+  JButton exitButton = new JButton();
+  JButton fetchMSB   = new JButton();
+  QtFrame qtf;
+  JLabel logoImage;
 
-   /**
-    * Creates a new <code>InfoPanel</code> instance.
-    *
-    * @param parent a <code>JFrame</code> value
-    * @param mqtm a <code>MSBQueryTableModel</code> value
-    */
-   public InfoPanel (MSBQueryTableModel msbQTM, Querytool qt, QtFrame qtf) {
-      msb_qtm = msbQTM;
-      localQuerytool = qt;
-      this.qtf=qtf;
-      //setSize(new Dimension(300, 550));
-      setMinimumSize(new Dimension(175, 550));
-      setPreferredSize(new Dimension(288, 550));
+  /**
+   * Creates a new <code>InfoPanel</code> instance.
+   *
+   * @param parent a <code>JFrame</code> value
+   * @param mqtm a <code>MSBQueryTableModel</code> value
+   */
+  public InfoPanel (MSBQueryTableModel msbQTM, Querytool qt, QtFrame qtf) {
+    msb_qtm = msbQTM;
+    localQuerytool = qt;
+    this.qtf=qtf;
+    //setSize(new Dimension(300, 550));
+    setMinimumSize(new Dimension(175, 550));
+    setPreferredSize(new Dimension(288, 550));
 
-      logoImage = new JLabel();
-      setBackground(Color.black);
-      MatteBorder matte = new MatteBorder(4,4,4,4,Color.green);
-      setBorder(matte);
-      GridBagLayout gbl = new GridBagLayout();
-      setLayout(gbl);
+    logoImage = new JLabel();
+    setBackground(Color.black);
+    MatteBorder matte = new MatteBorder(4,4,4,4,Color.green);
+    setBorder(matte);
+    GridBagLayout gbl = new GridBagLayout();
+    setLayout(gbl);
 
-      try {
-	 setImage();
-      } catch(Exception fnfe) {
-	 System.out.println("ERROR: "+fnfe.getMessage());
-      }
-      compInit();
-   }
+    try {
+      setImage();
+    } catch(Exception fnfe) {
+      System.out.println("ERROR: "+fnfe.getMessage());
+    }
+    compInit();
 
-   public void setImage() throws Exception {
-      URL url = new URL("file://"+LOGO_IMAGE);
-      if(url != null) {
-	 logoImage.setIcon(new ImageIcon(url));
-      }
-      else {
-	 logoImage.setIcon(new ImageIcon(InfoPanel.class.getResource("file://"+LOGO_IMAGE)));
-      }
-   }
+  }
 
-   /**
-    * Describe <code>compInit</code> method here.
-    *
-    */
-   public void compInit() {
-      GridBagConstraints gbc = new GridBagConstraints();
-      timePanel = new TimePanel();
+  public void setImage() throws Exception {
+    URL url = new URL("file://"+LOGO_IMAGE);
+    if(url != null) {
+      logoImage.setIcon(new ImageIcon(url));
+    }
+    else {
+      logoImage.setIcon(new ImageIcon(InfoPanel.class.getResource("file://"+LOGO_IMAGE)));
+    }
+  }
 
-      TelescopeDataPanel telescopeInfoPanel = new TelescopeDataPanel();
-      telescopeInfoPanel.config();
+  /**
+   * Describe <code>compInit</code> method here.
+   *
+   */
+  public void compInit() {
+    GridBagConstraints gbc = new GridBagConstraints();
+    timePanel = new TimePanel();
 
-      final Led blinker = new Led();
+    TelescopeDataPanel telescopeInfoPanel = new TelescopeDataPanel();
+    telescopeInfoPanel.config();
 
-      searchButton.setText("Search");
-      searchButton.setName("Search");
-      searchButton.setBackground(java.awt.Color.gray);
-      searchButton.addActionListener(new ActionListener() {
-	  public void actionPerformed(ActionEvent e) {
-	    final SwingWorker worker = new SwingWorker() {
-		Boolean isStatusOK;
+    final Led blinker = new Led();
 
-		public Object construct() {
-		  isStatusOK = new Boolean(localQuerytool.queryMSB());
-		  return isStatusOK;  //not used yet
-		}
+    searchButton.setText("Search");
+    searchButton.setName("Search");
+    searchButton.setBackground(java.awt.Color.gray);
+    searchButton.addActionListener(new ActionListener() {
+	public void actionPerformed(ActionEvent e) {
+	  final SwingWorker worker = new SwingWorker() {
+	      Boolean isStatusOK;
 
-		//Runs on the event-dispatching thread.
-		public void finished() { 
-		  blinker.blinkLed(false);
-		  Thread tableFill = new Thread(msb_qtm);
-		  tableFill.start();
-		}
-	      };
+	      public Object construct() {
+		isStatusOK = new Boolean(localQuerytool.queryMSB());
+		return isStatusOK;  //not used yet
+	      }
 
-	    blinker.blinkLed(true);
-	    //blinkThread.start();
-	    worker.start();  //required for SwingWorker 3
-	  }
-	} );
+	      //Runs on the event-dispatching thread.
+	      public void finished() { 
+		blinker.blinkLed(false);
+		Thread tableFill = new Thread(msb_qtm);
+		tableFill.start();
+	      }
+	    };
+
+	  blinker.blinkLed(true);
+	  //blinkThread.start();
+	  worker.start();  //required for SwingWorker 3
+	}
+      } );
       
-      fetchMSB.setText("Just get MSB 1");
-      searchButton.setBackground(java.awt.Color.gray);
-      fetchMSB.addActionListener(new ActionListener() {
-	    public void actionPerformed(ActionEvent e) {
-	       localQuerytool.fetchMSB(new Integer(1));
-	       qtf.buildStagingPanel();
-	    }
-	 } );
+    searchButton.setBackground(java.awt.Color.gray);
 
-      xmlPrintButton.setText("Execute");
-      xmlPrintButton.setName("Execute");
-      xmlPrintButton.setBackground(java.awt.Color.gray);
-      xmlPrintButton.addActionListener(this);
+    xmlPrintButton.setText("Execute");
+    xmlPrintButton.setName("Execute");
+    xmlPrintButton.setBackground(java.awt.Color.gray);
+    xmlPrintButton.addActionListener(this);
 
-      exitButton.setText("Exit");
-      exitButton.setName("Exit");
-      exitButton.setBackground(java.awt.Color.gray);
-      exitButton.addActionListener(this);
+    exitButton.setText("Exit");
+    exitButton.setName("Exit");
+    exitButton.setBackground(java.awt.Color.gray);
+    exitButton.addActionListener(this);
 
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.anchor = GridBagConstraints.EAST;
+    gbc.insets.bottom = 5;
+    gbc.insets.left = 10;
+    gbc.insets.right = 5;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(logoImage, gbc, 0, 0, 1, 1);
 
-      gbc.fill = GridBagConstraints.BOTH;
-      gbc.anchor = GridBagConstraints.EAST;
-      gbc.insets.bottom = 5;
-      gbc.insets.left = 10;
-      gbc.insets.right = 5;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(logoImage, gbc, 0, 0, 1, 1);
+    gbc.fill = GridBagConstraints.NONE;
+    gbc.anchor = GridBagConstraints.CENTER;
+    gbc.insets.bottom = 5;
+    gbc.insets.left = 5;
+    gbc.insets.right = 5;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(blinker, gbc, 0, 1, 1, 1);
 
-      gbc.fill = GridBagConstraints.NONE;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.insets.bottom = 5;
-      gbc.insets.left = 5;
-      gbc.insets.right = 5;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(blinker, gbc, 0, 1, 1, 1);
-
-      gbc.fill = GridBagConstraints.NONE;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.insets.bottom = 5;
-      gbc.insets.left = 5;
-      gbc.insets.right = 5;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(searchButton, gbc, 0, 2, 1, 1);
+    gbc.fill = GridBagConstraints.NONE;
+    gbc.anchor = GridBagConstraints.CENTER;
+    gbc.insets.bottom = 5;
+    gbc.insets.left = 5;
+    gbc.insets.right = 5;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(searchButton, gbc, 0, 2, 1, 1);
     
-      gbc.fill = GridBagConstraints.NONE;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(xmlPrintButton, gbc, 0, 3, 1, 1);
+    gbc.fill = GridBagConstraints.NONE;
+    gbc.anchor = GridBagConstraints.CENTER;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(xmlPrintButton, gbc, 0, 3, 1, 1);
 
-      gbc.fill = GridBagConstraints.NONE;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(exitButton, gbc, 0, 4, 1, 1);
+    gbc.fill = GridBagConstraints.NONE;
+    gbc.anchor = GridBagConstraints.CENTER;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(exitButton, gbc, 0, 4, 1, 1);
 
-      gbc.fill = GridBagConstraints.BOTH;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.weightx = 100;
-      gbc.weighty = 100;
-      add(telescopeInfoPanel, gbc, 0, 5, 1, 1);
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.anchor = GridBagConstraints.CENTER;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(telescopeInfoPanel, gbc, 0, 5, 1, 1);
 
-      gbc.fill = GridBagConstraints.BOTH;
-      gbc.weightx = 100;
-      gbc.weighty = 0;
-      hstLabel.setForeground(Color.black);
-      hstLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      gbc.insets.bottom = 0;
-      gbc.insets.top = 0;
-      add(hstLabel, gbc, 0, 6, 1, 1);
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.weightx = 100;
+    gbc.weighty = 0;
+    hstLabel.setForeground(Color.black);
+    hstLabel.setHorizontalAlignment(SwingConstants.CENTER);
+    gbc.insets.bottom = 0;
+    gbc.insets.top = 0;
+    add(hstLabel, gbc, 0, 6, 1, 1);
 
-      gbc.fill = GridBagConstraints.BOTH;
-      gbc.weightx = 100;
-      gbc.weighty = 0;
-      gbc.insets.left = 0;
-      gbc.insets.right = 0;
-      add(timePanel, gbc, 0, 7, 1, 1);
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.weightx = 100;
+    gbc.weighty = 0;
+    gbc.insets.left = 0;
+    gbc.insets.right = 0;
+    add(timePanel, gbc, 0, 7, 1, 1);
+  }
 
-      gbc.fill = GridBagConstraints.BOTH;
-      gbc.weightx = 100;
-      gbc.weighty = 0;
-      gbc.insets.left = 0;
-      gbc.insets.right = 0;
-      add(fetchMSB, gbc, 0, 8, 1, 1);
-   }
+  /**
+   * Describe <code>add</code> method here.
+   *
+   * @param c a <code>Component</code> value
+   * @param gbc a <code>GridBagConstraints</code> value
+   * @param x an <code>int</code> value
+   * @param y an <code>int</code> value
+   * @param w an <code>int</code> value
+   * @param h an <code>int</code> value
+   */
+  public void add(Component c, GridBagConstraints gbc, 
+		  int x, int y, int w, int h) {
+    gbc.gridx = x;
+    gbc.gridy = y;
+    gbc.gridwidth = w;
+    gbc.gridheight = h;
+    add(c, gbc);      
+  }
 
-   /**
-    * Describe <code>add</code> method here.
-    *
-    * @param c a <code>Component</code> value
-    * @param gbc a <code>GridBagConstraints</code> value
-    * @param x an <code>int</code> value
-    * @param y an <code>int</code> value
-    * @param w an <code>int</code> value
-    * @param h an <code>int</code> value
-    */
-   public void add(Component c, GridBagConstraints gbc, 
-		   int x, int y, int w, int h) {
-      gbc.gridx = x;
-      gbc.gridy = y;
-      gbc.gridwidth = w;
-      gbc.gridheight = h;
-      add(c, gbc);      
-   }
+  public String getXMLquery() {
+    return localQuerytool.getXML();
+  }
 
-   public String getXMLquery() {
-      return localQuerytool.getXML();
-   }
+  public String getImageFile() {
+    return LOGO_IMAGE;
+  }
 
-   public String getImageFile() {
-      return LOGO_IMAGE;
-   }
-
-   /**
-    * Describe <code>actionPerformed</code> method here.
-    *
-    * @param e an <code>ActionEvent</code> value
-    */
-   public void actionPerformed(ActionEvent e) {
-      Object source = e.getSource();
-      Color color = getBackground();
-      if (source == exitButton) {
-         System.exit(0);
-      }
-      if (source == searchButton) {
-         System.exit(0);
-      }
-      else if (source == xmlPrintButton) {
-	 localQuerytool.printXML();
-      }
-   }
+  /**
+   * Describe <code>actionPerformed</code> method here.
+   *
+   * @param e an <code>ActionEvent</code> value
+   */
+  public void actionPerformed(ActionEvent e) {
+    Object source = e.getSource();
+    Color color = getBackground();
+    if (source == exitButton) {
+      System.exit(0);
+    }
+    if (source == searchButton) {
+      System.exit(0);
+    }
+    else if (source == xmlPrintButton) {
+      localQuerytool.printXML();
+    }
+  }
 
 
 
--- src/main/edu/jach/qt/gui/OmpOM.java
+++ src/main/edu/jach/qt/gui/OmpOM.java
@@ -28,6 +28,7 @@ import orac.jcmt.iter.*;
 import orac.ukirt.inst.*;
 import orac.ukirt.iter.*;
 import orac.util.*;
+import java.util.Hashtable;
 
 /**
  * This is the top most class of the OMP-OM.  This 
@@ -39,157 +40,188 @@ import orac.util.*;
 public class OmpOM extends JPanel{
 
 
-   private ProgramTree	      ptree;
-   private File		      file;
-   private SpRootItem	      spRootItem;
+  private ProgramTree	      ptree;
+  private File		      file;
+  private SpRootItem	      spRootItem;
+  private Hashtable	      ptreeHashtable;
 
-   /**
-    * Creates a new <code>OmpOM</code> instance.
-    *
-    */
-   public OmpOM (){
-
-      OtFileIO.setXML(System.getProperty("OMP") != null);
-
-      //Load OM specific configs
-      QtTools.loadConfig(new String(System.getProperty("omCfg")));
-
-      /* Need to construct UKIRT-specific items so that their SpTypes are
-       * statically initialised.  Otherwise the sp classes won't know about 
-       * their types.  AB 19-Apr-2000
-       */
-
-      /* Init UKIRT Instruments */
-      SpItem spItem = new SpInstUFTI();
-      spItem = new SpInstCGS4();
-      spItem = new SpInstIRCAM3();
-      spItem = new SpInstMichelle();
-      spItem = new SpDRRecipe();
-
-      /* Init UKIRT SpTypes */
-      spItem = new SpIterBiasObs();
-      spItem = new SpIterBiasObs();
-      spItem = new SpIterCGS4();
-      spItem = new SpIterCGS4CalUnit();
-      spItem = new SpIterCGS4CalObs();
-      spItem = new SpIterCalUnit();
-      spItem = new SpIterDarkObs();
-      spItem = new SpIterFP();
-      spItem = new SpIterIRCAM3();
-      spItem = new SpIterIRPOL();
-      spItem = new SpIterMichelle();
-      spItem = new SpIterMichelleCalObs();
-      spItem = new SpIterNod();
-      spItem = new SpIterNodObs();
-      spItem = new SpIterUFTI();
-
-      /* Init GEMINI Miscellaneous */
-      spItem = new SpIterRepeat();
-      spItem = new SpIterOffset();
-      spItem = new SpIterObserve();
-      spItem = new SpIterSky();
-      spItem = new SpSchedConstObsComp();
-      spItem = new SpSiteQualityObsComp();
-
-      /* Init JCMT Instruments */
-      spItem = new SpInstSCUBA();
-      spItem = new SpInstHeterodyne();
-
-      /* Init JCMT SpTypes */
-      spItem = new SpIterFocusObs();
-      spItem = new SpIterFrequency();
-      spItem = new SpIterJiggleObs();
-      spItem = new SpIterRasterObs();
-      spItem = new SpIterSkydipObs();
-      spItem = new SpIterStareObs();
-      spItem = new SpIterScanObs();
-      spItem = new SpIterPointingObs();
-      spItem = new SpIterPhotomObs();
-
-      /* Init JCMT Miscellaneous */
-      //spItem = new orac.jcmt.obsComp.SpSiteQualityObsComp();
-
-      startOM();
-   }
-
-   private void startOM() {
-
-      file = new File(System.getProperty("msbFile"));
-      spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
-
-      ptree = new ProgramTree();
-      ptree.addTree(spRootItem);
-      ptree.setMinimumSize(new Dimension(400,550) );
-
-   }
-
-   public void resetTree() {
-      spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
-      ptree.addTree(spRootItem);
-   }
-
-   public JSplitPane getTreePanel() {
-      
-      CalibrationArea ca = new CalibrationArea();
-      JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
-					    ptree, ca);
-      return splitPane;
-   }
-
-   public JSplitPane getDragTreePanel() {
-      
-      DragDropObject ddo = new DragDropObject(spRootItem);
-      MsbNode root = new MsbNode(ddo);
-      getItems(spRootItem, root);
-
-      DnDJTree ddt = new DnDJTree(root);
-      ddt.setCellRenderer(new DragTreeCellRenderer());
-      //JPanel p = new JPanel();
-      JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
-					    ptree, ddt);
-
-      return splitPane;
-   }
-
-   public void test() {
-      
-      JFrame f = new JFrame();
-      f.setSize(400,300);
-      DragDropObject ddo = new DragDropObject(spRootItem);
-      MsbNode root = new MsbNode(ddo);
-      getItems(spRootItem, root);
-      DnDJTree ddt = new DnDJTree(root);
-      ddt.setCellRenderer(new MyTreeCellRenderer());
-      f.getContentPane().add(ddt);
-      //f.pack();
-      f.show();
-   }
-
-
-   /** public void getItems (SpItem spItem,DefaultMutableTreeNode node)
-       is a public method to add ALL the items of a sp object into the
-       JTree *recursively*.
-      
-       @param SpItem spItem,DefaultMutableTreeNode node
-       @return  none
-       @throws none
-      
+  /**
+   * Creates a new <code>OmpOM</code> instance.
+   *
    */
-   private void getItems (SpItem spItem, MsbNode node) {
-      Enumeration children = spItem.children();
-      while (children.hasMoreElements()) {
+  public OmpOM (){
+    ptreeHashtable = new Hashtable();
+
+    //Load OM specific configs
+    OtFileIO.setXML(System.getProperty("OMP") != null);
+    QtTools.loadConfig(new String(System.getProperty("omCfg")));
+
+    /* Need to construct UKIRT-specific items so that their SpTypes are
+     * statically initialised.  Otherwise the sp classes won't know about 
+     * their types.  AB 19-Apr-2000
+     */
+
+    /* Init UKIRT Instruments */
+    SpItem spItem = new SpInstUFTI();
+    spItem = new SpInstCGS4();
+    spItem = new SpInstIRCAM3();
+    spItem = new SpInstMichelle();
+    spItem = new SpDRRecipe();
+
+    /* Init UKIRT SpTypes */
+    spItem = new SpIterBiasObs();
+    spItem = new SpIterBiasObs();
+    spItem = new SpIterCGS4();
+    spItem = new SpIterCGS4CalUnit();
+    spItem = new SpIterCGS4CalObs();
+    spItem = new SpIterCalUnit();
+    spItem = new SpIterDarkObs();
+    spItem = new SpIterFP();
+    spItem = new SpIterIRCAM3();
+    spItem = new SpIterIRPOL();
+    spItem = new SpIterMichelle();
+    spItem = new SpIterMichelleCalObs();
+    spItem = new SpIterNod();
+    spItem = new SpIterNodObs();
+    spItem = new SpIterUFTI();
+
+    /* Init GEMINI Miscellaneous */
+    spItem = new SpIterRepeat();
+    spItem = new SpIterOffset();
+    spItem = new SpIterObserve();
+    spItem = new SpIterSky();
+    spItem = new SpSchedConstObsComp();
+    spItem = new SpSiteQualityObsComp();
+
+    /* Init JCMT Instruments */
+    spItem = new SpInstSCUBA();
+    spItem = new SpInstHeterodyne();
+
+    /* Init JCMT SpTypes */
+    spItem = new SpIterFocusObs();
+    spItem = new SpIterFrequency();
+    spItem = new SpIterJiggleObs();
+    spItem = new SpIterRasterObs();
+    spItem = new SpIterSkydipObs();
+    spItem = new SpIterStareObs();
+    spItem = new SpIterScanObs();
+    spItem = new SpIterPointingObs();
+    spItem = new SpIterPhotomObs();
+
+    /* Init JCMT Miscellaneous */
+    //spItem = new orac.jcmt.obsComp.SpSiteQualityObsComp();
+
+  }
+
+  /**
+   * Describe <code>addNewTree</code> method here. This adds a
+   * <code>ProgramTree</code>, referrenced by the msbID, to the list
+   * of trees.
+   *
+   * @param msbID an <code>int</code> value
+   */
+  public void addNewTree(Integer msbID) {
+
+    file = new File(System.getProperty("msbFile"));
+    spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
+     
+    ptree = new ProgramTree();
+    ptree.addTree(spRootItem);
+    ptree.setMinimumSize(new Dimension(400,550) );
+
+    ptreeHashtable.put(msbID, ptree);
+  }
+
+  /**
+   * Describe <code>addNewTree</code> method here.
+   * The method used for debugging.  It loads in a hard-wired MSB file
+   * to use as the ProgramTree object.
+   */
+  public void addNewTree() {
+    file = new File(System.getProperty("arrayTests"));
+    spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
+     
+    ptree = new ProgramTree();
+    ptree.addTree(spRootItem);
+    ptree.setMinimumSize(new Dimension(400,550) );
+
+    ptreeHashtable.put(new Integer(41), ptree);
+  }
+
+  public void resetTree() {
+    spRootItem = OtFileIO.fetchSp(file.getParent(), file.getName());
+    ptree.addTree(spRootItem);
+  }
+
+  public JSplitPane getTreePanel(Integer msbID) {
+      
+    CalibrationArea ca = new CalibrationArea();
+    JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
+					  (ProgramTree)ptreeHashtable.get(msbID), ca);
+    return splitPane;
+  }
+
+  public JSplitPane getDragTreePanel() {
+      
+    DragDropObject ddo = new DragDropObject(spRootItem);
+    MsbNode root = new MsbNode(ddo);
+    getItems(spRootItem, root);
+
+    DnDJTree ddt = new DnDJTree(root);
+    ddt.setCellRenderer(new DragTreeCellRenderer());
+    //JPanel p = new JPanel();
+    JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
+					  ptree, ddt);
+
+    return splitPane;
+  }
+
+  public void test() {
+      
+    JFrame f = new JFrame();
+    f.setSize(400,300);
+    DragDropObject ddo = new DragDropObject(spRootItem);
+    MsbNode root = new MsbNode(ddo);
+    getItems(spRootItem, root);
+    DnDJTree ddt = new DnDJTree(root);
+    ddt.setCellRenderer(new MyTreeCellRenderer());
+    f.getContentPane().add(ddt);
+    //f.pack();
+    f.show();
+  }
+
+
+  /** public void getItems (SpItem spItem,DefaultMutableTreeNode node)
+      is a public method to add ALL the items of a sp object into the
+      JTree *recursively*.
+      
+      @param SpItem spItem,DefaultMutableTreeNode node
+      @return  none
+      @throws none
+      
+  */
+  private void getItems (SpItem spItem, MsbNode node) {
+    Enumeration children = spItem.children();
+    while (children.hasMoreElements()) {
 	 
-	 SpItem  childNode = (SpItem) children.nextElement();
-	 DragDropObject ddo = new DragDropObject(childNode);
-	 MsbNode temp = new MsbNode(ddo);
-	 node.add(temp);
-	 getItems(childNode,temp);
-      }
-   }
+      SpItem  childNode = (SpItem) children.nextElement();
+      DragDropObject ddo = new DragDropObject(childNode);
+      MsbNode temp = new MsbNode(ddo);
+      node.add(temp);
+      getItems(childNode,temp);
+    }
+  }
 
 
-   public static void main(String[] args) {
-      QtTools.loadConfig("/home/mrippa/netroot/install/omp/QT/config/qt.conf");
-      OmpOM om = new OmpOM();
-   }
+  public static void main(String[] args) {
+    QtTools.loadConfig(System.getProperty("qtConfig"));
+    QtTools.loadConfig(System.getProperty("omConfig"));
+
+    JFrame f = new JFrame();
+    OmpOM om = new OmpOM();
+    om.addNewTree();
+    f.getContentPane().add(om.getTreePanel(new Integer(41)));
+    f.setSize(400, 300);
+    f.setVisible(true);
+  }
 }// OmpOM
