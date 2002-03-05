81d2f1a5883b01d4649cfeddf0be250531b5eaeb
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-03-05 22:23:41 +0000

    Controls the look of the SpObs list.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@1964 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/utils/ObsListCellRenderer.java
@@ -0,0 +1,50 @@
+package edu.jach.qt.utils;
+
+import om.util.OracColor;
+import gemini.sp.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+/**
+ * ObsListCellRenderer.java
+ *
+ *
+ * Created: Mon Mar  4 15:05:01 2002
+ *
+ * @author <a href="mailto: mrippa@jach.hawaii.edu"Mathew Rippa</a>
+ */
+
+
+public class ObsListCellRenderer extends DefaultListCellRenderer {
+  final static ImageIcon obsIcon = new ImageIcon(System.getProperty("IMAG_PATH")+"observation.gif");
+  
+  // This is the only method defined by ListCellRenderer.
+  // We just reconfigure the JLabel each time we're called.
+  
+  public Component getListCellRendererComponent(JList list,
+						Object value,            // value to display
+						int index,               // cell index
+						boolean isSelected,      // is the cell selected
+						boolean cellHasFocus)    // the list and the cell have the focus
+  {
+    String s = ((SpObs)value).getTitle();
+    setText(s);
+    setIcon(obsIcon);
+
+    if (isSelected) {
+      setForeground(OracColor.blue);
+      //setBackground(list.getSelectionBackground());
+      //setForeground(list.getSelectionForeground());
+    }
+
+    else {
+      setBackground(list.getBackground());
+      setForeground(list.getForeground());
+    }
+
+    setEnabled(list.isEnabled());
+    setFont(list.getFont());
+    return this;
+  }
+}// ObsListCellRenderer
