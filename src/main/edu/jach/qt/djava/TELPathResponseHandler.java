0d4e3389f46a3f6fe488ece70fed5081bb8ffbd6
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-07-02 08:37:18 +0000

    Airmass callback
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@2983 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/djava/TELPathResponseHandler.java
@@ -0,0 +1,78 @@
+package edu.jach.qt.djava;
+
+import au.gov.aao.drama.*;
+import ocs.utils.CommandReceiver;
+import org.apache.log4j.Logger;
+
+/**
+ * <code>CSOPathResponseHandler</code> This class is used to
+ * handle responses to the GetPath() method.
+ *
+ * @author <a href="mailto:mrippa@jach.hawaii.edu">Mathew Rippa</a>
+ * $Id$ */
+public class TELPathResponseHandler extends DramaPath.ResponseHandler {
+
+  static Logger logger = Logger.getRootLogger();
+
+  private CommandReceiver cr;
+  public TELPathResponseHandler(DramaPath p, CommandReceiver cr) {
+    super(p);
+    this.cr = cr;
+    logger.debug(logger.getClass().getName());
+  }
+
+  /** 
+   * Sucess is invoked when we have completed the get path operation.
+   */
+  public boolean Success(DramaPath path, DramaTask task) throws DramaException {
+    
+    // Informational message
+    //task.MsgOut("Got path to task "+path.TaskName() +".");
+    logger.info("Got path to task "+path.TaskName() +".");
+    
+    // Start the monitor operation.
+    DramaMonitor Monitor = new DramaMonitor(path, new QT_MonResponse(cr), true, "AIRMASS");
+
+    // We have sent a new message, so return true.
+    return true;
+  }
+
+  /** 
+   * Invoked if the GetPath operation fails
+   */
+  public boolean Error(DramaPath path, DramaTask task)  throws DramaException {
+    DramaStatus status = task.GetEntStatus();
+    logger.warn("Failed to get path to task \"" + path + "\"");
+    logger.warn("Failed with status - " + status);
+
+    cr.setPathLock(false);
+
+    return false;
+  }
+
+}
+
+/*
+ * $Log$
+ * Revision 1.1  2002/07/02 08:37:18  mrippa
+ * Airmass callback
+ *
+ * Revision 1.6  2002/04/20 02:52:17  mrippa
+ * Updated Log
+ *
+ * Revision 1.5  2002/04/20 02:41:24  mrippa
+ * Added log4j functionality.
+ *
+ * Revision 1.4  2002/04/01 21:55:37  mrippa
+ * Modified the setTaskLock() name to setPathLock()
+ *
+ * Revision 1.3  2002/03/07 20:27:43  mrippa
+ * Unlock the CommandReceiver on error.!
+ *
+ * Revision 1.2  2002/03/05 22:12:10  mrippa
+ * Provides CommanderReceiver to callback to.
+ *
+ * Revision 1.1  2002/02/24 06:55:06  mrippa
+ * Added Drama support for monitoring tau values.
+ *
+ */
