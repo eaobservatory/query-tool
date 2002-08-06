e00d7e112d6a64366565fc7adb45a6a9a5080ffe
Shaun Dewitt <s.dewitt@jach.hawaii.edu>
2002-08-06 18:50:56 +0000

    New class providing execution method for JCMT.  Should be run in a thread.  Success/failure is denoted by a file .success or .failure in the deferred directory.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@3418 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/gui/ExecuteJCMT.java
@@ -0,0 +1,126 @@
+package edu.jach.qt.gui;
+
+import edu.jach.qt.utils.*;
+
+import gemini.sp.*;
+import gemini.sp.obsComp.*;
+
+import orac.jcmt.inst.*;
+import orac.jcmt.iter.*;
+import orac.jcmt.obsComp.*;
+import orac.ukirt.inst.*;
+import orac.ukirt.iter.*;
+import orac.util.*;
+
+import om.console.*;
+import om.util.*;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import javax.swing.*;
+
+import ocs.utils.*;
+
+import org.apache.log4j.Logger;
+
+
+public class ExecuteJCMT extends Execute implements Runnable {
+
+    static Logger logger = Logger.getLogger(ExecuteJCMT.class);
+    private SpItem _itemToExecute;
+    private static String jcmtDir = File.separator +
+	"jcmtdata" + File.separator + "orac_data";
+    
+    public ExecuteJCMT(SpItem item) throws Exception {
+	_itemToExecute = item;
+    };
+
+    public void run() {
+	// To execute JCMT, we write the execution to a file
+	File success = new File ("/jcmtdata/orac_data/deferred/.success");
+	File failure = new File ("/jcmtdata/orac_data/deferred/.failure");
+	success.delete();
+	failure.delete();
+	try {
+	    success.createNewFile();
+	    failure.createNewFile();
+	}
+	catch (IOException ioe) {
+	    logger.error("Unable to create success/fail file");
+	    return;
+	}
+
+	File file = new File (jcmtDir + File.separator + "ExecuteMe.xml");
+	try {
+	    FileWriter writer = new FileWriter (file);
+	    writer.write(_itemToExecute.toXML());
+	    writer.close();
+	}
+	catch (IOException ioe) {
+	    logger.error("Error writing translation file");
+	    file.delete();
+	    success.delete();
+	    return;
+	}
+
+	// Now send this file as an argument to the translate process
+	String translator = System.getProperty("jcmtTranslator");
+	if (translator == null) {
+	    logger.error("No transation process defined");
+	    file.delete();
+	    success.delete();
+	    return;
+	}
+	byte [] odfFile = new byte [1024];
+	Runtime rt;
+	// Do the translation
+	try {
+	    rt = Runtime.getRuntime();
+	    String command = translator +" "+ file.getPath();
+	    System.out.println("Running command "+command);
+	    Process p = rt.exec(command);
+	    InputStream istream = p.getInputStream();
+	    istream.read(odfFile);
+	    System.out.println(new String (odfFile));
+	    int rtn = p.waitFor();
+	    System.out.println("Process returned with exit status "+rtn);
+	}
+	catch (InterruptedException ie) {
+	    logger.error ("Translation exited prematurely...", ie);
+	    file.delete();
+	    success.delete();
+	    return;
+	}
+	catch (IOException ioe) {
+	    logger.error("Error executing translator...", ioe);
+	    file.delete();
+	    success.delete();
+	    return;
+	}
+	file.delete();
+
+	if (TelescopeDataPanel.DRAMA_ENABLED) {
+	    try {
+		rt = Runtime.getRuntime();
+// 		command = "/home/dewitt/bin/loadSCUQUEUE.ksh "+ new String (odfFile);
+		String command = "/jac_sw/omp/QT/bin/loadSCUQUEUE.ksh "+ new String (odfFile);
+		System.out.println ("Running command "+command+" &");
+		Process p = rt.exec(command);
+		int rtn = p.exitValue();
+		System.out.println("Process returned with exit status "+rtn);
+		if (rtn != 0) {
+		    logger.error("Non-zero exit condition returned from LOADQ - "+rtn);
+		    success.delete();
+		}
+	    }
+	    catch (IOException ioe) {
+		logger.error("Error executing LOADQ...", ioe);
+		success.delete();
+		return;
+	    }
+	}
+	failure.delete();
+	return;
+    }
+}
