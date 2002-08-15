d30bd48da05522ed0fd1f15d0628f2defb9d6537
Shaun Dewitt <s.dewitt@jach.hawaii.edu>
2002-08-15 18:16:20 +0000

    Class which displays a frame when the user selects the "View" button.  Displays a list of column names and allows users to select a subset for view.  By default, the msbid and checksum are always hidden.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@3660 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/gui/ColumnSelector.java
@@ -0,0 +1,71 @@
+package edu.jach.qt.gui;
+
+import java.awt.event.*;
+import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.lang.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class ColumnSelector 
+    extends JFrame 
+    implements ActionListener {
+
+    private JPanel columnPanel;
+    private MSBQueryTableModel _msbqtm;
+
+    public ColumnSelector() {
+    }
+
+    public ColumnSelector (MSBQueryTableModel model) {
+	// Disable the parent
+	_msbqtm = model;
+	this.setSize(150,300);
+	this.getContentPane().setLayout(new BorderLayout());
+	this.addCheckBoxes();
+	this.addCloseButton();
+	this.setVisible(true);
+    }
+
+    private void addCheckBoxes( ) {
+	columnPanel = new JPanel(new GridLayout(0,1));
+	String [] colNames = XmlUtils.getColumnNames(_msbqtm.MSB_SUMMARY);
+	BitSet bits = _msbqtm.getBitSet();
+	for (int i=0; i<colNames.length; i++) {
+	    JCheckBox cb = new JCheckBox(colNames[i]);
+	    if (bits.get(i)) {
+		cb.setSelected(true);
+	    }
+	    columnPanel.add(cb);
+	}
+	this.getContentPane().add(columnPanel, BorderLayout.CENTER);
+    }
+
+    private void addCloseButton() {
+	JButton closeButton = new JButton ("Accept");
+	closeButton.setLocation(375, 275);
+	closeButton.addActionListener(this);
+	this.getContentPane().add(closeButton, BorderLayout.SOUTH);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+	BitSet selected = new BitSet(_msbqtm.getRealColumnCount());
+	System.out.println("Number of components: "+columnPanel.getComponentCount());
+	for (int i=0; i<columnPanel.getComponentCount(); i++) {
+	    if (columnPanel.getComponent(i) instanceof JCheckBox) {
+		JCheckBox cb = (JCheckBox)columnPanel.getComponent(i);
+		if (cb.isSelected()) {
+		    selected.set(i);
+		}
+		else {
+		    selected.clear(i);
+		}
+	    }
+	}
+
+	_msbqtm.updateColumns(selected);
+	this.dispose();
+    }
+}
