2d0404cbd65f3567da095daf2b6ac939a748c1ee
Shaun Dewitt <s.dewitt@jach.hawaii.edu>
2002-08-30 18:26:18 +0000

    New class
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@3845 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/gui/ProjectTableModel.java
@@ -0,0 +1,82 @@
+package edu.jach.qt.gui;
+
+import java.io.*;
+import java.util.Vector;
+import javax.swing.event.*;
+import javax.swing.table.*;
+import javax.xml.parsers.*;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;  
+
+public class ProjectTableModel 
+    extends AbstractTableModel
+    implements Runnable,  TableModelListener {
+
+    static Logger logger = Logger.getLogger(ProjectTableModel.class);
+    private static final String ROOT_ELEMENT_TAG = "SpMSBSummary";
+    private static final String MSB_SUMMARY = System.getProperty("msbSummary")+"."+System.getProperty("user.name");
+
+    private static String [] colName = {
+	"projectid", "priority"};
+    private static Class [] colClass = {
+	String.class, Integer.class};
+
+    private Vector projectIds = new Vector();
+    private Vector priorities = new Vector();
+
+    private Document doc;
+    private boolean  docIsNull = true;;
+
+    public ProjectTableModel() {
+ 	addTableModelListener(this);
+    }
+
+    public void run () {
+
+	fireTableChanged(null);
+    }
+
+    public int getColumnCount() {
+	return colName.length;
+    }
+
+    public Class getColumnClass(int index) {
+	return colClass[index];
+    }
+
+    public String getColumnName(int index) {
+	return colName[index];
+    }
+
+    public int getRowCount() {
+	return projectIds.size();
+    }
+
+    public Object getValueAt(int r, int c) {
+	if (r < 0 || projectIds.size() == 0) {
+	    return "----";
+	}
+	if (colName[c].equalsIgnoreCase("projectid")) {
+	    return projectIds.elementAt(r);
+	}
+	else if (colName[c].equalsIgnoreCase("priority")) {
+	    return priorities.elementAt(r);
+	}
+	else {
+	    return "---";
+	}
+    }
+
+    public void setValueAt(Object value, int r, int c) {
+    }
+
+    public void tableChanged(TableModelEvent evt) {
+	projectIds.clear();
+	priorities.clear();
+	Vector [] data = XmlUtils.getProjectData();
+	projectIds = data[0];
+	priorities = data[1];
+    }
+}
---
+++ src/main/edu/jach/qt/gui/ProjectTableSelectionModel.java
@@ -0,0 +1,31 @@
+package edu.jach.qt.gui;
+
+import java.util.*;
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.event.*;
+
+
+class ProjectTableSelectionModel 
+    extends DefaultListSelectionModel
+    implements ListSelectionListener {
+
+    QtFrame _qtf;
+
+    public ProjectTableSelectionModel (QtFrame parent) {
+	_qtf = parent;
+	this.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
+	addListSelectionListener(this);
+	this.setSelectionInterval(0,0);
+
+    }
+
+    public void valueChanged(ListSelectionEvent e) {
+	// Get the MSBQueryTableModel
+	String projectID = (String)_qtf.getProjectModel().getValueAt(getMinSelectionIndex(), 0);
+	if (projectID == null || projectID.equals("") || projectID.startsWith("-")) return;
+	_qtf.updateColumnSizes();
+	_qtf.getModel().setProjectId(projectID);
+
+	_qtf.setColumnSizes();
+    }
+}
