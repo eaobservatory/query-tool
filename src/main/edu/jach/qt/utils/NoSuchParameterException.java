ed82dc1f1434226ef0d4483ec7598e476d7c7888
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-10-18 19:17:06 +0000

    START
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@4343 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

---
+++ src/main/edu/jach/qt/utils/NoSuchParameterException.java
@@ -0,0 +1,21 @@
+package edu.jach.qt.app;
+
+/**
+ * NoSuchParameterException.java
+ *
+ *
+ * Created: Fri Oct 11 16:54:53 2002
+ *
+ * @author <a href="mailto: mrippa@jach.hawaii.edu"Mathew Rippa</a>
+ */
+
+public class NoSuchParameterException extends Exception {
+  public NoSuchParameterException (){
+  }
+
+  public NoSuchParameterException (String message){
+    super (message);
+  }
+
+  
+}// NoSuchParameterException
