ead3f7da8847c163a7ddc7ee33b4486c150b9fbb
Matt Rippa <m.rippa@jach.hawaii.edu>
2002-10-25 22:47:41 +0000

    Mostly cleaned comments. Now oosTest is static method if QtTools.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@4361 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/ExecuteUKIRT.java
+++ src/main/edu/jach/qt/gui/ExecuteUKIRT.java
@@ -1,11 +1,5 @@
 package edu.jach.qt.gui;
 
-
-
-
-
-
-
 import edu.jach.qt.utils.*;
 import gemini.sp.*;
 import gemini.sp.obsComp.*;
@@ -26,176 +20,152 @@ import org.apache.log4j.Logger;
 
 public class ExecuteUKIRT extends Execute implements Runnable {
 
-    static Logger logger = Logger.getLogger(ExecuteUKIRT.class);
+  static Logger logger = Logger.getLogger(ExecuteUKIRT.class);
+
+  private String myInst;
+
     
-    public ExecuteUKIRT() throws Exception {
-    };
+  public ExecuteUKIRT() throws Exception {
+  };
 
-    public void run () {
-	System.out.println("Starting execution...");
-	File success = new File ("/ukirtdata/orac_data/deferred/.success");
-	File failure = new File ("/ukirtdata/orac_data/deferred/.failure");
-	success.delete();
-	failure.delete();
-	try {
-	    success.createNewFile();
-	    failure.createNewFile();
-	}
-	catch (IOException ioe) {
-	    logger.error("Unable to create success/fail file");
-	    return;
-	}
-
-	SpItem itemToExecute;
-	if (!isDeferred) {
-	    itemToExecute = ProgramTree.selectedItem;
-	    logger.info("Executing observation from Program List");
-	}
-	else {
-	    itemToExecute = DeferredProgramList.currentItem;
-	    logger.info("Executing observation from deferred list");
-	}
-
-	//SequenceManager scm = SequenceManager.getHandle();
-	
-	SpItem inst = (SpItem) SpTreeMan.findInstrument(itemToExecute);
-	if (inst == null) {
-	    logger.error("No instrument found");
-	    success.delete();
-	    return;
-	}
-	
-	// *TODO* Replace this crap!
-	Translating tFlush = new Translating();
-	tFlush.start();
-	
-	String tname = QtTools.translate(itemToExecute, inst.type().getReadable());
-	JFrame frame =  tFlush.getFrame();
-	if (frame == null) {
-	    logger.error("Failed to get frame...");
-	    success.delete();
-	    return;
-	}
-	frame.hide();
-	tFlush.stop();
-	
-	// Catch null sequence names - probably means translation
-	// failed:
-	if (tname == null) {
-	    //new ErrorBox ("Translation failed. Please report this!");
-	    logger.error("Translation failed. Please report this!");
-	    success.delete();
-	    return;
-	}else{
-	    logger.info("Trans OK");
-	}
-	
-	
-	// Prevent IRCAM3 and CGS4 from running together
-	// figure out if the same inst. is already in use or
-	// whether IRCAM3 and CGS4 would be running together
-	//SequenceConsole console;
-	//Vector consoleList = scm.getConsoleList().getList();
-
-	//for(int i=0; i<consoleList.size(); i++) {
-	//console = (SequenceConsole)consoleList.elementAt(i);
-	    
-	  if ( true ) {
-	    System.out.println("Sending the oosTest");
-	    
-	    //int status = QtTools.execute(new String[] {"/jac_sw/omp/QT/bin/oosTest",inst.type().getReadable()}, false, true);
-	    
-	    //System.out.println("Status is: "+status);
-	    
-	    //if(inst.type().getReadable().equals(console.getInstrument())) {
-	    //logger.info("TNAME IS: "+tname);
-
-	    //console.resetObs(itemToExecute.getTitle(), tname);
-	    //JOptionPane dialog = new JOptionPane();
-	    //int selection = dialog.showConfirmDialog
-	    //(null,"New Observation for: "+inst.type().getReadable(), "Comfirmation",
-	    // JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null );
-	
-	    //if(selection==dialog.NO_OPTION) {
-	    //return;
-	    //}
-		
-	    //if(selection==dialog.YES_OPTION) {
-	      //Run script that calls 'oosLoad OOS_<itemToExecute.getTitle()> <tname>'
-	    //}
-	    String myInst = inst.type().getReadable();
-	    String[] cmd = {"/jac_sw/omp/QT/bin/oosTest","OOS_"+myInst};
-	    
-	    int status = -1;
-	    try {
-	      Process p = Runtime.getRuntime().exec(cmd);
-	      BufferedReader stdOut = 
-		new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-	      String s = null;
-	      while ((s = stdOut.readLine()) != null) {
-		System.err.println(s);
-		if ( s.endsWith("NOT RUNNING")) {
-		  status = 0;
-		}
-		else if ( s.endsWith("IS RUNNING")) {
-		  status = 1;
-		}
-	      }
-	    } catch (IOException e) {
-		logger.error("StdOut got IO exception:"+e.getMessage());
-	    }
-	    
-	    if ( status == 0 ) {
-	      System.out.println("not running... send oosLoad");
-	    }
-	    else {
-	      System.out.println("instrument running ... loadORAC deferred!");
-
-	      String strippedTname = tname.substring(0, tname.indexOf("."));
-	      String [] myCmd = {"ditscmd", "OOS_"+myInst, "load", strippedTname};
-	      logger.info("Sending command: "
-			  +myCmd[0]
-			  +" "+myCmd[1] 
-			  +" "+myCmd[2] 
-			  +" "+myCmd[3] );
-
-	      int loadStatus = QtTools.execute(myCmd, false, true);
-
-	      failure.delete();
-	      return;
-	    }
-    	  }
-	  
-	  // 	  if(inst.type().getReadable().equals("IRCAM3") && 
-	  // 	     console.getInstrument().equals("CGS4")) {
-	  // 	    JOptionPane.showMessageDialog (null,
-	  // 					   "IRCAM3 and CGS4 cannot run at the same time.",
-	  // 					   "",
-	  // 					   JOptionPane.ERROR_MESSAGE);
-	  // 	    success.delete();
-	  // 	    return;
-	  // 	  }
-
-	  // 	  if(inst.type().getReadable().equals("CGS4") && 
-	  // 	     console.getInstrument().equals("IRCAM3")) {
-	  // 	    JOptionPane.showMessageDialog (null,
-	  // 					   "IRCAM3 and CGS4 cannot run at the same time.",
-	  // 					   "",
-	  // 					   JOptionPane.ERROR_MESSAGE);
-	  // 	    success.delete();
-	  // 	    return;
-	  // 	  }
-	  //}
-
-	if ( System.getProperty("os.name").equals("SunOS") && TelescopeDataPanel.DRAMA_ENABLED) {
-	    if (QtTools.loadDramaTasks(inst.type().getReadable()) == 0){
-		failure.delete();
-	    }
-	    else {
-		success.delete();
-	    }
-	}
-	
+  public void run () {
+    System.out.println("Starting execution...");
+    File success = new File ("/ukirtdata/orac_data/deferred/.success");
+    File failure = new File ("/ukirtdata/orac_data/deferred/.failure");
+    success.delete();
+    failure.delete();
+    try {
+      success.createNewFile();
+      failure.createNewFile();
     }
+    catch (IOException ioe) {
+      logger.error("Unable to create success/fail file", ioe);
+      return;
+    }
+
+    SpItem itemToExecute;
+    if (!isDeferred) {
+      itemToExecute = ProgramTree.selectedItem;
+      logger.info("Executing observation from Program List");
+    }
+    else {
+      itemToExecute = DeferredProgramList.currentItem;
+      logger.info("Executing observation from deferred list");
+    }
+	
+    SpItem inst = (SpItem) SpTreeMan.findInstrument(itemToExecute);
+    if (inst == null) {
+      logger.error("No instrument found");
+      success.delete();
+      return;
+    }
+	
+    myInst = inst.type().getReadable();
+    String[] oosStateCmd = {"/jac_sw/omp/QT/bin/oosTest", 
+			    " -instrument", 
+			    " OOS_"+myInst, 
+			    " -state", 
+			    " State"};
+
+    // If this oos is all ready in a Running state then abort.
+    if ( QtTools.oosTest(oosStateCmd) == 1) {
+      logger.error(myInst +" is Running. Aborting Execution.");
+      success.delete();
+      return;
+    } 
+    
+
+    // *TODO* Replace this crap!
+    Translating tFlush = new Translating();
+    tFlush.start();
+	
+    String tname = QtTools.translate(itemToExecute, inst.type().getReadable());
+    JFrame frame =  tFlush.getFrame();
+    if (frame == null) {
+      logger.error("Failed to get frame...");
+      success.delete();
+      return;
+    }
+    frame.hide();
+    tFlush.stop();
+	
+    // Catch null sequence names - probably means translation
+    // failed:
+    if (tname == null) {
+      //new ErrorBox ("Translation failed. Please report this!");
+      logger.error("Translation failed. Please report this!");
+      success.delete();
+      return;
+    }else{
+      logger.info("Trans OK");
+    }
+	
+	
+    // Prevent IRCAM3 and CGS4 from running together
+    // figure out if the same inst. is already in use or
+    // whether IRCAM3 and CGS4 would be running together
+    //SequenceConsole console;
+    //Vector consoleList = scm.getConsoleList().getList();
+
+    //for(int i=0; i<consoleList.size(); i++) {
+    //console = (SequenceConsole)consoleList.elementAt(i);
+	    
+    //if ( true ) {
+    logger.debug("Sending the oosTest");
+
+    String[] oosActiveCmd = {"/jac_sw/omp/QT/bin/oosTest", " -instrument", " OOS_"+myInst, " -active"};
+
+    if ( QtTools.oosTest(oosActiveCmd) == 0 ) {
+      logger.debug("not running... send oosLoad");
+    }
+    else {
+      logger.debug("instrument running ... loadORAC deferred!");
+
+      String strippedTname = tname.substring(0, tname.indexOf("."));
+      String [] myCmd = {"ditscmd", "OOS_"+myInst, "load", strippedTname};
+      logger.info("Sending command: "
+		  +myCmd[0]
+		  +" "+myCmd[1] 
+		  +" "+myCmd[2] 
+		  +" "+myCmd[3] );
+
+      int loadStatus = QtTools.execute(myCmd, false, true);
+
+      failure.delete();
+      return;
+    }
+    //}
+	  
+    // 	  if(inst.type().getReadable().equals("IRCAM3") && 
+    // 	     console.getInstrument().equals("CGS4")) {
+    // 	    JOptionPane.showMessageDialog (null,
+    // 					   "IRCAM3 and CGS4 cannot run at the same time.",
+    // 					   "",
+    // 					   JOptionPane.ERROR_MESSAGE);
+    // 	    success.delete();
+    // 	    return;
+    // 	  }
+
+    // 	  if(inst.type().getReadable().equals("CGS4") && 
+    // 	     console.getInstrument().equals("IRCAM3")) {
+    // 	    JOptionPane.showMessageDialog (null,
+    // 					   "IRCAM3 and CGS4 cannot run at the same time.",
+    // 					   "",
+    // 					   JOptionPane.ERROR_MESSAGE);
+    // 	    success.delete();
+    // 	    return;
+    // 	  }
+    //}
+
+    if ( System.getProperty("os.name").equals("SunOS") && TelescopeDataPanel.DRAMA_ENABLED) {
+      if (QtTools.loadDramaTasks(inst.type().getReadable()) == 0) {
+	failure.delete();
+      }
+      else {
+	success.delete();
+      }
+    }
+	
+  }
+
 }
