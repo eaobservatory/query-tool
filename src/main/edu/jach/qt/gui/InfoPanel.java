05be569e48304986cd4f25bd7e9a0125a6a290d2
Shaun Dewitt <s.dewitt@jach.hawaii.edu>
2003-05-03 01:52:24 +0000

    Modified Search button action listener so that the Search button is disabled while processing the results.  This gets over a problem of parsing the results when the search button is pressed again while processing.
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@5819 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/InfoPanel.java
+++ src/main/edu/jach/qt/gui/InfoPanel.java
@@ -31,219 +31,223 @@ import org.apache.log4j.Logger;
  */
 public class InfoPanel extends JPanel implements ActionListener {
 
-  static Logger logger = Logger.getLogger(InfoPanel.class);
+    static Logger logger = Logger.getLogger(InfoPanel.class);
 
-  /**
-   * The constant <code>LOGO_IMAGE</code> specifies the String
-   * location for the QT logo image.
-   * 
-   */
-  public static final String LOGO_IMAGE = System.getProperty("qtLogo");
+    /**
+     * The constant <code>LOGO_IMAGE</code> specifies the String
+     * location for the QT logo image.
+     * 
+     */
+    public static final String LOGO_IMAGE = System.getProperty("qtLogo");
 
-  /**
-   * The constant <code>SAT_WEBPAGE</code> specifies the webpage
-   * containing the String of the latest image to show.
-   * 
-   */
-  public static final String SAT_WEBPAGE = System.getProperty("satellitePage");
+    /**
+     * The constant <code>SAT_WEBPAGE</code> specifies the webpage
+     * containing the String of the latest image to show.
+     * 
+     */
+    public static final String SAT_WEBPAGE = System.getProperty("satellitePage");
 
-  /**
-   * The constant <code>IMG_PREFIX</code> is the static portion of the
-   * image source URL.
-   * 
-   */
-  public static final String IMG_PREFIX = System.getProperty("imagePrefix");
+    /**
+     * The constant <code>IMG_PREFIX</code> is the static portion of the
+     * image source URL.
+     * 
+     */
+    public static final String IMG_PREFIX = System.getProperty("imagePrefix");
 
-  /**
-   * The variable <code>searchButton</code> is the button clicked to
-   * start a query.
-   * 
-   */
-  public static JButton searchButton   = new JButton();
+    /**
+     * The variable <code>searchButton</code> is the button clicked to
+     * start a query.
+     * 
+     */
+    public static JButton searchButton   = new JButton();
 
-  /**
-   * The variable <code>logoPanel</code> is a reference to the easily get the LogoPanel.
-   *
-   */
-  public static LogoPanel logoPanel    = new LogoPanel();
+    /**
+     * The variable <code>logoPanel</code> is a reference to the easily get the LogoPanel.
+     *
+     */
+    public static LogoPanel logoPanel    = new LogoPanel();
 
-  private TelescopeDataPanel telescopeInfoPanel;
-  private MSBQueryTableModel msb_qtm;
-  private TimePanel timePanel;
-  private UTPanel   utPanel;
-  private SatPanel satPanel;
-  private Querytool localQuerytool;
-  private QtFrame qtf;
-  //private JButton xmlPrintButton;
-  private JButton exitButton;
-  private JButton fetchMSB;
-  private Timer   queryExpiredTimer;
+    private TelescopeDataPanel telescopeInfoPanel;
+    private MSBQueryTableModel msb_qtm;
+    private TimePanel timePanel;
+    private UTPanel   utPanel;
+    private SatPanel satPanel;
+    private Querytool localQuerytool;
+    private QtFrame qtf;
+    //private JButton xmlPrintButton;
+    private JButton exitButton;
+    private JButton fetchMSB;
+    private Timer   queryExpiredTimer;
 
 
-  /**
-   * Creates a new <code>InfoPanel</code> instance.
-   *
-   * @param msbQTM a <code>MSBQueryTableModel</code> value
-   * @param qt a <code>Querytool</code> value
-   * @param qtFrame a <code>QtFrame</code> value
-   */
-  public InfoPanel (MSBQueryTableModel msbQTM, Querytool qt, QtFrame qtFrame) {
-    localQuerytool = qt;
-    msb_qtm = msbQTM;
-    qtf = qtFrame;
+    /**
+     * Creates a new <code>InfoPanel</code> instance.
+     *
+     * @param msbQTM a <code>MSBQueryTableModel</code> value
+     * @param qt a <code>Querytool</code> value
+     * @param qtFrame a <code>QtFrame</code> value
+     */
+    public InfoPanel (MSBQueryTableModel msbQTM, Querytool qt, QtFrame qtFrame) {
+	localQuerytool = qt;
+	msb_qtm = msbQTM;
+	qtf = qtFrame;
 
-    MatteBorder matte = new MatteBorder(3,3,3,3, Color.green);
-    GridBagLayout gbl = new GridBagLayout();
+	MatteBorder matte = new MatteBorder(3,3,3,3, Color.green);
+	GridBagLayout gbl = new GridBagLayout();
 
-    setBackground(Color.black);
-    setBorder(matte);
-    setLayout(gbl);
-    setMinimumSize(new Dimension(174, 450));
-    setPreferredSize(new Dimension(174, 450));
-    setMaximumSize(new Dimension(174, 450));
+	setBackground(Color.black);
+	setBorder(matte);
+	setLayout(gbl);
+	setMinimumSize(new Dimension(174, 450));
+	setPreferredSize(new Dimension(174, 450));
+	setMaximumSize(new Dimension(174, 450));
 
-    compInit();
+	compInit();
 
-  }
+    }
 
-  private void compInit() {
-    GridBagConstraints gbc = new GridBagConstraints();
+    private void compInit() {
+	GridBagConstraints gbc = new GridBagConstraints();
 
-    //xmlPrintButton	= new JButton();
-    exitButton		= new JButton();
-    fetchMSB		= new JButton();
-    timePanel		= new TimePanel();
-    utPanel		= new UTPanel();
-    satPanel		= new SatPanel();
-    telescopeInfoPanel	= new TelescopeDataPanel(this);
+	//xmlPrintButton	= new JButton();
+	exitButton		= new JButton();
+	fetchMSB		= new JButton();
+	timePanel		= new TimePanel();
+	utPanel		= new UTPanel();
+	satPanel		= new SatPanel();
+	telescopeInfoPanel	= new TelescopeDataPanel(this);
 
-    /*Setup the SEARCH button*/
-    InfoPanel.searchButton.setText("Search");
-    InfoPanel.searchButton.setName("Search");
-    InfoPanel.searchButton.addActionListener(new ActionListener() {
-	public void actionPerformed(ActionEvent e) {
-	    qtf.updateColumnSizes();
-	    qtf.getModel().clear();
-	    qtf.getProjectModel().clear();
-	    qtf.repaint(0);
+	/*Setup the SEARCH button*/
+	InfoPanel.searchButton.setText("Search");
+	InfoPanel.searchButton.setName("Search");
+	InfoPanel.searchButton.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    qtf.updateColumnSizes();
+		    qtf.getModel().clear();
+		    qtf.getProjectModel().clear();
+		    qtf.repaint(0);
+		    searchButton.setEnabled(false);
 
-	  final SwingWorker worker = new SwingWorker() {
-	      Boolean isStatusOK;
+		    final SwingWorker worker = new SwingWorker() {
+			    Boolean isStatusOK;
 
-	      public Object construct() {
-		isStatusOK = new Boolean(localQuerytool.queryMSB());
-		return isStatusOK;  //not used yet
-	      }
+			    public Object construct() {
+				isStatusOK = new Boolean(localQuerytool.queryMSB());
+				return isStatusOK;  //not used yet
+			    }
 
-	      //Runs on the event-dispatching thread.
-	      public void finished() { 
-		if ( isStatusOK.booleanValue()) {
-// 		  qtf.updateColumnSizes();
+			    //Runs on the event-dispatching thread.
+			    public void finished() { 
+				if ( isStatusOK.booleanValue()) {
+				    // 		  qtf.updateColumnSizes();
 
- 		  Thread tableFill = new Thread(msb_qtm);
-		  tableFill.start();
-		  try {
-		      tableFill.join();
-		  }
-		  catch (InterruptedException iex) {
-		      logger.warn("Problem joining tablefill thread");
-		  };
+				    Thread tableFill = new Thread(msb_qtm);
+				    tableFill.start();
+				    try {
+					tableFill.join();
+				    }
+				    catch (InterruptedException iex) {
+					logger.warn("Problem joining tablefill thread");
+				    };
 
-		  Thread projectFill = new Thread (qtf.getProjectModel());
-		  projectFill.start();
-		  try {
-		      projectFill.join();
-		  }
-		  catch (InterruptedException iex) {
-		      logger.warn("Problem joining projectfill thread");
-		  };
+				    synchronized (this) {
+					Thread projectFill = new Thread (qtf.getProjectModel());
+					projectFill.start();
+					try {
+					    projectFill.join();
+					}
+					catch (InterruptedException iex) {
+					    logger.warn("Problem joining projectfill thread");
+					};
 
-		  qtf.initProjectTable();
-		  msb_qtm.setProjectId("All");
-		  qtf.setColumnSizes();
-		  logoPanel.stop();
-		  qtf.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-		  if (queryExpiredTimer != null) {
-		      queryExpiredTimer.cancel();
-		  }
-		  queryExpiredTimer = new Timer();
-		  qtf.setQueryExpired (false);
-		  System.out.println("Query expiration: "+System.getProperty("queryTimeout"));
-		  Integer timeout = new Integer(System.getProperty("queryTimeout"));
-		  if (timeout.intValue() != 0) {
-		      int delay = timeout.intValue() * 60 * 1000; // Conversion from minutes of milliseconds
-		      queryExpiredTimer.schedule(new QueryExpiredTask(), delay);
-		  }
+					qtf.initProjectTable();
+				    }
+				    msb_qtm.setProjectId("All");
+				    qtf.setColumnSizes();
+				    logoPanel.stop();
+				    qtf.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				    if (queryExpiredTimer != null) {
+					queryExpiredTimer.cancel();
+				    }
+				    queryExpiredTimer = new Timer();
+				    qtf.setQueryExpired (false);
+				    System.out.println("Query expiration: "+System.getProperty("queryTimeout"));
+				    Integer timeout = new Integer(System.getProperty("queryTimeout"));
+				    if (timeout.intValue() != 0) {
+					int delay = timeout.intValue() * 60 * 1000; // Conversion from minutes of milliseconds
+					queryExpiredTimer.schedule(new QueryExpiredTask(), delay);
+				    }
+				}
+				searchButton.setEnabled(true);
+			    }
+			};
+		    logger.info("Query Sent");
+
+		    localQuerytool.printXML();
+		    qtf.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+		    logoPanel.start();
+		    worker.start();  //required for SwingWorker 3
 		}
-	      }
-	    };
-	  logger.info("Query Sent");
+	    } );
 
-	  localQuerytool.printXML();
-	  qtf.setCursor(new Cursor(Cursor.WAIT_CURSOR));
-	  logoPanel.start();
-	  worker.start();  //required for SwingWorker 3
-	}
-      } );
+	InfoPanel.searchButton.setBackground(java.awt.Color.gray);
 
-    InfoPanel.searchButton.setBackground(java.awt.Color.gray);
+	/*
+	  xmlPrintButton.setText("Fetch MSB");
+	  xmlPrintButton.setName("Fetch MSB");
+	  xmlPrintButton.setBackground(java.awt.Color.gray);
+	  xmlPrintButton.addActionListener(this);
+	*/
+	fetchMSB.setText("Fetch MSB");
+	fetchMSB.setName("Fetch MSB");
+	fetchMSB.setBackground(java.awt.Color.gray);
+	fetchMSB.addActionListener(this);
 
-    /*
-    xmlPrintButton.setText("Fetch MSB");
-    xmlPrintButton.setName("Fetch MSB");
-    xmlPrintButton.setBackground(java.awt.Color.gray);
-    xmlPrintButton.addActionListener(this);
-    */
-    fetchMSB.setText("Fetch MSB");
-    fetchMSB.setName("Fetch MSB");
-    fetchMSB.setBackground(java.awt.Color.gray);
-    fetchMSB.addActionListener(this);
+	/*Setup the EXIT button*/
+	exitButton.setText("Exit");
+	exitButton.setName("Exit");
+	exitButton.setBackground(java.awt.Color.gray);
+	exitButton.addActionListener(this);
 
-    /*Setup the EXIT button*/
-    exitButton.setText("Exit");
-    exitButton.setName("Exit");
-    exitButton.setBackground(java.awt.Color.gray);
-    exitButton.addActionListener(this);
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.NORTH;
+	gbc.weighty = 0.0;
+	add(logoPanel, gbc, 0, 0, 1, 1);
 
-    gbc.fill = GridBagConstraints.BOTH;
-    gbc.anchor = GridBagConstraints.NORTH;
-    gbc.weighty = 0.0;
-    add(logoPanel, gbc, 0, 0, 1, 1);
-
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.anchor = GridBagConstraints.CENTER;
-    gbc.insets = new Insets(2,15,2,15);
-    gbc.weighty = 0.0;
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.insets = new Insets(2,15,2,15);
+	gbc.weighty = 0.0;
         
-    /*Add all the buttons*/
-    add(InfoPanel.searchButton, gbc, 0, 5, 1, 1);
-    add(fetchMSB, gbc, 0, 10, 1, 1);
-    add(exitButton, gbc, 0, 15, 1, 1);
+	/*Add all the buttons*/
+	add(InfoPanel.searchButton, gbc, 0, 5, 1, 1);
+	add(fetchMSB, gbc, 0, 10, 1, 1);
+	add(exitButton, gbc, 0, 15, 1, 1);
 
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.anchor = GridBagConstraints.CENTER;
-    gbc.weightx = 100;
-    gbc.insets = new Insets(0,2,0,2);
-    gbc.weighty = 0;
-    add(telescopeInfoPanel, gbc, 0, 20, 1, 1);
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.insets = new Insets(0,2,0,2);
+	gbc.weighty = 0;
+	add(telescopeInfoPanel, gbc, 0, 20, 1, 1);
 
-    add(satPanel, gbc, 0, 30, 1, 1);
+	add(satPanel, gbc, 0, 30, 1, 1);
 
 
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.weightx = 100;
-    gbc.weighty = 0;
-    gbc.insets.left = 0;
-    gbc.insets.right = 0;
-    add(timePanel, gbc, 0, 40, 1, 1);
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	gbc.insets.left = 0;
+	gbc.insets.right = 0;
+	add(timePanel, gbc, 0, 40, 1, 1);
 
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.weightx = 100;
-    gbc.weighty = 0;
-    gbc.insets.left = 0;
-    gbc.insets.right = 0;
-    add(utPanel, gbc, 0, 60, 1, 1);
-  }
+	gbc.fill = GridBagConstraints.HORIZONTAL;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	gbc.insets.left = 0;
+	gbc.insets.right = 0;
+	add(utPanel, gbc, 0, 60, 1, 1);
+    }
 
     /**
      * Get the parent frame.
@@ -261,24 +265,24 @@ public class InfoPanel extends JPanel implements ActionListener {
 	return localQuerytool;
     }
 
-  private void add(Component c, GridBagConstraints gbc, 
-		  int x, int y, int w, int h) {
-    gbc.gridx = x;
-    gbc.gridy = y;
-    gbc.gridwidth = w;
-    gbc.gridheight = h;
-    add(c, gbc);      
-  }
+    private void add(Component c, GridBagConstraints gbc, 
+		     int x, int y, int w, int h) {
+	gbc.gridx = x;
+	gbc.gridy = y;
+	gbc.gridwidth = w;
+	gbc.gridheight = h;
+	add(c, gbc);      
+    }
 
-  /**
-   * <code>getXMLquery</code> will get the String that contains the
-   * current XML defining the query.
-   *
-   * @return a <code>String</code> representing the query. 
-   */
-  public String getXMLquery() {
-    return localQuerytool.getXML();
-  }
+    /**
+     * <code>getXMLquery</code> will get the String that contains the
+     * current XML defining the query.
+     *
+     * @return a <code>String</code> representing the query. 
+     */
+    public String getXMLquery() {
+	return localQuerytool.getXML();
+    }
 
     /**
      * Get the <code>TelescopeDataPanel</code>
@@ -297,46 +301,46 @@ public class InfoPanel extends JPanel implements ActionListener {
 	return satPanel;
     }
 
-  /* -- No longer use-- now a static const
-    public String getImageFile() {
-    return LOGO_IMAGE;
-    }
-  */
+    /* -- No longer use-- now a static const
+       public String getImageFile() {
+       return LOGO_IMAGE;
+       }
+    */
 
-  /*
-   * <code>getCSODcHub</code> method here.
-   *
-   * @return a <code>DcHub</code> value
+    /*
+     * <code>getCSODcHub</code> method here.
+     *
+     * @return a <code>DcHub</code> value
    
-   public DcHub getCSODcHub() {
-   return telescopeInfoPanel.getHub();
-   }
-  */
+     public DcHub getCSODcHub() {
+     return telescopeInfoPanel.getHub();
+     }
+    */
 
-  /**
-   * <code>actionPerformed</code> satisfies the ActionListener
-   * interface.  This is called when any ActionEvents are triggered by
-   * registered ActionListeners. In this case it's either the exit
-   * button or the fetchMSB button.
-   *
-   * @param e an <code>ActionEvent</code> value 
-   */
-  public void actionPerformed(ActionEvent e) {
-    Object source = e.getSource();
-    Color color = getBackground();
-    if (source == exitButton) {
+    /**
+     * <code>actionPerformed</code> satisfies the ActionListener
+     * interface.  This is called when any ActionEvents are triggered by
+     * registered ActionListeners. In this case it's either the exit
+     * button or the fetchMSB button.
+     *
+     * @param e an <code>ActionEvent</code> value 
+     */
+    public void actionPerformed(ActionEvent e) {
+	Object source = e.getSource();
+	Color color = getBackground();
+	if (source == exitButton) {
 
-      if (TelescopeDataPanel.DRAMA_ENABLED) {
+	    if (TelescopeDataPanel.DRAMA_ENABLED) {
 
-	telescopeInfoPanel.closeHub();
-      }
+		telescopeInfoPanel.closeHub();
+	    }
 
-      qtf.exitQT();
+	    qtf.exitQT();
+	}
+	else if (source == fetchMSB) {
+	    qtf.sendToStagingArea();
+	}
     }
-    else if (source == fetchMSB) {
-      qtf.sendToStagingArea();
-    }
-  }
 
     public class QueryExpiredTask extends TimerTask {
 	public void run() {
