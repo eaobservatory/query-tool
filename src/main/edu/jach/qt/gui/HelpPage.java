d8e52f71d4622ecad44279b4266d9ec9268a0a82
Sam Hart <s.hart@jach.hawaii.edu>
2007-05-22 01:37:44 +0000

    Make help page more useful by not relying on browsers that might not be there -> SH
    git-svn-id: file:///jac_sw/svnroot/scicom/trunk/omp/java/qt@11320 64693ad5-8cdc-4012-857f-4cd9bf50b8c8

--- src/main/edu/jach/qt/gui/HelpPage.java
+++ src/main/edu/jach/qt/gui/HelpPage.java
@@ -1,9 +1,7 @@
 package edu.jach.qt.gui;
 
-import java.lang.*;
-import java.io.*;
-import javax.swing.*;
-
+import java.io.File;
+import edu.jach.qt.utils.HTMLViewer ;
 
 /**
  * Display the current help information in an external browser.
@@ -24,75 +22,24 @@ import javax.swing.*;
  * @author $Author$
  * @version $Id$
  */
-class HelpPage {
-
-    private String helpFile;
-    private final String [] browserList = {"netscape",
-					   "opera",
-					   "mosaic",
-					   "xemacs -f w3",
-					   "mozilla"
-    };
-
-    /**
-     * Constructor.
-     * Attempts to display the help page in a browser.
-     */
-    public HelpPage() {
-	File tmp  = new File (System.getProperty("qtConfig"));
-	String dir = tmp.getAbsoluteFile().getParent();
-	helpFile = dir + File.separator + "Documents/AboutTheQT.html";
-	String browser = System.getProperty("Browser");
-	String command = browser + " file://" + helpFile;
-	Runtime rt = Runtime.getRuntime();
-	try {
-	    Process proc = rt.exec(command);
-	    rt.addShutdownHook(new ShutdownThread(proc));
+class HelpPage
+{
+	/**
+	 * Constructor.
+	 * Attempts to display the help page in a browser.
+	 */
+	public HelpPage()
+	{
+		File tmp = new File( System.getProperty( "qtConfig" ) );
+		String dir = tmp.getAbsoluteFile().getParent();
+		String helpFile = dir + File.separator + "Documents/AboutTheQT.html";
+		
+		HTMLViewer htmlViewer = new HTMLViewer( null , helpFile ) ;
 	}
-	catch (IOException ioe) {
-	    Process proc = this.tryAlternate(rt);
-	    if (proc != null) {
-		rt.addShutdownHook(new ShutdownThread(proc));
-	    }
-	}
-    }
 
-    private Process tryAlternate(Runtime rt) {
-	int i;
-	Process proc = null;
- 	for (i=0; i<browserList.length; i++ ) {
-	    String command = browserList[i] + " file://" + helpFile;
-	    try {
-		proc = rt.exec(command);
-		break;
-	    }
-	    catch (IOException ioe) {continue;}
+	public static void main( String[] args )
+	{
+		HelpPage helpPage = new HelpPage();
 	}
-	if (i == browserList.length) {
-	    JOptionPane.showMessageDialog(null, 
-					  "No suitable browser found",
-					  "ERROR - No Browser",
-					  JOptionPane.ERROR_MESSAGE);
-	}
-	return proc;
-    }
-
-    class ShutdownThread extends Thread {
-	private Process _proc;
-	ShutdownThread(Process proc) {
-	    _proc = proc;
-	}
-	public void run () {
-	    _proc.destroy();
-	}
-    }
-
-    public static void main (String [] args) {
-	System.setProperty("Browser", "/usr/lib/mozilla/mozilla-bin");
-	System.setProperty("Browser", "xemacs -f w3");
-	System.setProperty("Browser", "fred");
-	HelpPage helpPage = new HelpPage();
-    }
-
 
 }
